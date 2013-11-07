110eb9d04a75f52437d5fcc2d0d822aa06678cb8
Arjun <arjuntvd@gmail.com>
2013-11-07 10:43:30 -0500

    updates

--- scripts/python/nb_plot_simstats.py
+++ scripts/python/nb_plot_simstats.py
@@ -21,6 +21,10 @@ filename = os.path.join(datadir, "simstats_vm.csv")
 dfss = pandas.read_csv(filename, na_values=['\n'])
 #print df
 
+# <markdowncell>
+
+# ### Group dfss by q2wbin column
+
 # <codecell>
 
 dfss_grpd_q2wbin = dfss.groupby('Q2Wbin')
@@ -31,7 +35,51 @@ print ngridx, ngridy
 
 # <markdowncell>
 
-# ### Test Division between columns of two different DFs
+# ### Test gr_nFthVq2wbin
+
+# <codecell>
+
+from __future__ import division
+
+#create Figure
+plt.figure(figsize=(8,8))
+# create Axes that go in the Figure
+ax1 = plt.subplot(3,1,1,ylabel = 'nFth') 
+ax2 = plt.subplot(3,1,2,ylabel='nEsr')
+ax3 = plt.subplot(3,1,3,xlabel='q2wbin',ylabel='frc')
+#sels = {}
+nf = [[],[],[],[]]
+ne = [[],[],[],[]]
+frc = [[],[],[],[]]
+q2wb = []
+for q2wbin in dfss_grpd_q2wbin.groups:
+    #print 'q2wbin', q2wbin
+    df = dfss_grpd_q2wbin.get_group(q2wbin)
+    #print 'sim:'
+    #print df['Sim']
+    
+    siml = df['Sim'].max()
+    selt1 = (df['Sim']==siml) & (df['Top']==1)
+    selt2 = (df['Sim']==siml) & (df['Top']==2)
+    #nFth_siml = df.nFth(sel)
+    #nFth = df.nFth[sels[2]]
+    #nEsr = df.nEsr[sels[2]]
+    f = nEsr/nFth
+    #print siml,nFth_siml
+    nf[0].append(df.nFth[sels[1]])
+    ne[0].append(df.nEsr[sels[1]])
+    frc.append(f)
+    q2wb.append(q2wbin)
+    
+ax1.scatter(q2wb,nf)
+ax2.scatter(q2wb,ne)
+ax3.scatter(q2wb,frc)
+#ax3.axes.xaxis.label = 'Q2WBIN'
+plt.show()
+
+# <codecell>
+
+### Test Division Operation between two different columumns of DF
 
 # <codecell>
 
@@ -57,34 +105,4 @@ for q2wbin in dfss_grpd_q2wbin.groups:
         
         print dfsel
         print df[(df['Top']==2) & (df['Varset']==1)]
-
-# <markdowncell>
-
-# ### Test gr_nFthVq2wbin
-
-# <codecell>
-
-from __future__ import division
-dfss_grpd_q2wbin = dfss.groupby('Q2Wbin')
-nq2wbins = len(dfss_grpd_q2wbin.groups)
-ngridx = 4
-ngridy = nq2wbins/ngridx
-print ngridx, ngridy
-
-#gr_FthVq2wbin
-
-for q2wbin in dfss_grpd_q2wbin.groups:
-    print 'q2wbin', q2wbin
-    df = dfss_grpd_q2wbin.get_group(q2wbin)
-    #print 'sim:'
-    #print df['Sim']
-    
-    siml = df['Sim'].max()
-    sel = (df['Sim']==siml) & (df['Top']==2)
-    #nFth_siml = df.nFth(sel)
-    nFth_siml = df.nFth[sel]
-    print siml,nFth_siml
-
-# <codecell>
-
 
--- scripts/python/nb_test.py
+++ scripts/python/nb_test.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
+# <markdowncell>
+
+# ## Lists and Arrays in Python (Arrays are made up of Lists in Python)
+
+# <markdowncell>
+
+# Example of List & implementation of 2D data structure using List
+
+# <codecell>
+
+l = [] #Create an empty List
+print "Empty list = ",l
+l = [1,2,3] #Initialize List
+print "Initialized list =",l
+
+md = [[],[]] #Create a List of Lists (="2D Array")
+print "Empty \"2D Array\" = ",md
+md[0]=[1,2,3] #Initialize "2D Array"=(Lists of Lists)
+md[1]=[4,5,6]
+print "Initiliazed \"2D Array\" = ",md
+#print md[1][1] #Access a particular "2D Array" element
+
+
+# <rawcell>
+
+# Direct use of Numpy to create Arrays
+
+# <codecell>
+
+
 # <codecell>
 
 import matplotlib.pyplot as plt
