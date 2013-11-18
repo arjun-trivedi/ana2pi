37678a739cb54d4343015a996fa5369756cbc987
Arjun <arjuntvd@gmail.com>
2013-11-18 12:38:19 -0500

    updates

--- scripts/python/pyth-test.py
+++ scripts/python/pyth-test.py
@@ -102,45 +102,60 @@ outdir = os.path.join(anadir,'polobs.new')
 # print z
 
 """ Use Case for storing Pandas DataFrame with ROOT Histograms in 
-a HDF5 file"""
+a HDF5 file
+Instructions from: http://pandas.pydata.org/pandas-docs/dev/io.html#hdf5-pytables
+
+"""
 import numpy as np
 randn = np.random.randn
 import pandas as pd
 
 from ROOT import TH1D
 
-store = pd.HDFStore('store.h5')
-print 'store =',store
+def make_test_hdf5():
+	print 'Going to create store.h5:'
+	#1. If store.hdf5 exists, delete it
+	if os.path.isfile('store.h5'):
+		os.remove('store.h5')
+	#2. Create HDF5 store & see what it contains
+	store = pd.HDFStore('store.h5')
+	print 'store =',store
 
+	#3. Create Histograms to store
+	hl = []
+	for i in range(0,8):
+		name = 'h%d'%(i+1)
+		hl.append(TH1D(name,name,100,0,5))
+		
+	index = np.arange(0,8)
+	df = pd.DataFrame()
 
-# h1 = TH1D("test1","test1",100,0,5)
-# h2 = TH1D("test2","test2",100,0,5)
-# h3 = TH1D("test3","test3",100,0,5)
-# hl = [h1,h2,h3]
-# hl = []
-# for i in range(0,8):
-# 	name = 'h%d'%(i+1)
-# 	hl.append(TH1D(name,name,100,0,5))
-# hl[0].Draw()
-# print hl[0]
+	#4. Insert 1st columns into df
+	if not df:
+		data = pd.DataFrame({'s1':hl},index=index) # Data for 1st. Column 
+		df = df.append(data)
 
+	#5. Insert 2nd (duplicate) column into df
+	df['H'] = hl
+	#print 'df=',df
 
+	#6. Add df to store
+	store['df']=df
+	#7. See the stored data type 
+	print 'Type of stored data = ',store.root.df._v_attrs.pandas_type
+	#8. See what the file finally contains
+	print store
+
+def read_test_hdf5():
+	print 'Going to read contents of store.h5:'
+	if os.path.isfile('store.h5'):
+		store = pd.HDFStore('store.h5')
+		print 'store =',store
+		print store['df']
+	else:
+		print 'store.h5 does not exist!'
 	
-# #index = pd.date_range('1/1/2000', periods=8)
-# index = np.arange(0,8)
-# # s = pd.Series(randn(5), index=['a', 'b', 'c', 'd', 'e'])
-# #df = pd.DataFrame(randn(8, 3), index=index,
-# #                  columns=['A', 'B', 'C'])
-# df = pd.DataFrame()
-# if not df:
-# 	data = pd.DataFrame({'s1':hl},index=index) # Data for 1st. Column 
-# 	df = df.append(data)
 
-# df['H'] = hl
-# print 'df=',df
-
-# store['df']=df
-# #store.root.wp._v_attrs.pandas_type
-# store
-
-print store['df']
+make_test_hdf5()
+read_test_hdf5()
+#print store['df']
