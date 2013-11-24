2893267c0032782ec82fd621d9646940b3f4600d
Arjun <arjuntvd@gmail.com>
2013-11-24 10:44:48 -0500

    updates

--- scripts/python/plot_simstats.py
+++ scripts/python/plot_simstats.py
@@ -24,10 +24,10 @@ print 'nq2wbins=',nq2wbins
 
 MAX_TOPS,MIN_TOPS = range(2)
 NTOPS=5
-tops = [1,2,3,4,5];
-top_labels = ['Top1','Top2','Top3','Top4','Top5']
-top_markers = ['s','<','8' ,'p','>']
-top_colors =  ['r','k','r','r','k']
+TOPS = [1,2,3,4,5];
+TOP_LABELS = ['Top1','Top2','Top3','Top4','Top5']
+TOP_MARKERS = ['s','<','8' ,'p','>']
+TOP_COLORS =  ['r','k','r','r','k']
 
 NSTATS=4
 nFB_ST,nEB_SR,nFB_SR,nEB_SA = range(0,NSTATS)
@@ -49,17 +49,17 @@ def plot_track_stats():
                 tl.set_color('r')
         
             lns = []
-            for top in tops:
+            for top in TOPS:
                 itop=top-1
                 if top==2 or top==5:
                     #print df['Sim'][df['Top']==top]
                     sel= (df_q2wbinnum['Top']==top)
                     lns.append(axs[MAX_TOPS].scatter(df_q2wbinnum['Sim'][sel],df_q2wbinnum[STATS_NAME[stat]][sel],color='k',
-                                     marker=top_markers[itop],label=top_labels[itop]))
+                                     marker=TOP_MARKERS[itop],label=TOP_LABELS[itop]))
                 else:
                    sel= (df_q2wbinnum['Top']==top)
                    lns.append(axs[MIN_TOPS].scatter(df_q2wbinnum['Sim'][sel],df_q2wbinnum[STATS_NAME[stat]][sel],color='r',
-                                    marker=top_markers[itop],label=top_labels[itop]))
+                                    marker=TOP_MARKERS[itop],label=TOP_LABELS[itop]))
             labs = [l.get_label() for l in lns]
             axs[MAX_TOPS].legend(lns, labs, loc=2)
                     
@@ -69,7 +69,49 @@ def plot_track_stats():
                 os.makedirs(outdir)
             #plt.figure(stat)
             fig.savefig('%s/%s.jpg'%(outdir,STATS_NAME[stat]))
-            del fig
-            del axs
-            
-plot_track_stats()
+
+def plot_latest_stats():
+    for stat in range(0,NSTATS):
+        fig=plt.figure('%s'%(STATS_NAME[stat]))
+        ax = []
+        ax.append(plt.subplot(1,1,1,title=STATS_NAME[stat]))
+        ax.append(ax[MAX_TOPS].twinx())
+        ax[MAX_TOPS].set_ylabel('%s t2,5'%STATS_NAME[stat])
+        ax[MIN_TOPS].set_ylabel(('%s t1,3,4'%STATS_NAME[stat]),color='r')
+        for tl in ax[MIN_TOPS].get_yticklabels():
+            tl.set_color('r')
+
+        #Fill nbins[top][q2wbin]
+        nbins = np.zeros((NTOPS,nq2wbins));
+        q2wbs = np.arange(1,nq2wbins+1,1);
+        for q2wbinnum in df_grpd_q2wbinnum.groups:
+            #print 'q2wbinnum=',q2wbinnum
+            iq2wbin = q2wbinnum-1
+            df_q2wbinnum = df_grpd_q2wbinnum.get_group(q2wbinnum)
+            siml = df_q2wbinnum['Sim'].max()
+            for top in TOPS:
+                itop = top-1
+                sel = (df_q2wbinnum['Sim']==siml) & (df_q2wbinnum['Top']==(top))
+                nbins[itop][iq2wbin] = df_q2wbinnum[STATS_NAME[stat]][sel]
+    
+        lns = []
+        for top in TOPS:
+            itop = top-1
+            nbins_top=nbins[itop]
+            if top==2 or top==5:
+                lns.append(ax[MAX_TOPS].scatter(q2wbs, nbins_top, color='k', marker=TOP_MARKERS[itop], s=50,
+                       label=TOP_LABELS[itop]))
+            else:
+                lns.append(ax[MIN_TOPS].scatter(q2wbs, nbins_top, color='r', marker=TOP_MARKERS[itop], s=50,
+                       label=TOP_LABELS[itop]))    
+        labs = [l.get_label() for l in lns]
+        ax[MAX_TOPS].legend(lns, labs, loc=2)
+
+        #savefig
+        outdir = os.path.join(anadir,'simstats.new','cum')
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir)
+        fig.savefig('%s/%s.jpg'%(outdir,STATS_NAME[stat]))
+  
+#plot_track_stats()
+plot_latest_stats()
