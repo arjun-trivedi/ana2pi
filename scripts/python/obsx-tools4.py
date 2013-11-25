c3dbfc74eb0417e0bbd05b104b65f8a9e0be8507
Arjun <arjuntvd@gmail.com>
2013-11-25 15:45:37 -0500

    updates

--- scripts/python/obsx-tools4.py
+++ scripts/python/obsx-tools4.py
@@ -1,4 +1,5 @@
 #added extraction of 1D
+#made creating of DF use more of vectorized operations
 from __future__ import division
 
 import os
@@ -49,32 +50,33 @@ NSEQ=9
 ST,SR,SA,SC,SH,SF,ER,EC,EF=range(NSEQ)
 SEQ_NAME=['ST','SR','SA','SC','SH','SF','ER','EC','EF']
 
-NPOLS=4
-POS,NEG,UNP,AVG=range(NPOLS)
-POLS_COLOR=['kRed','kBlack','kBlue','kMagenta']
-POLS_NAME=['POS','NEG','UNP','AVG']
+NPOLS=3
+POS,NEG,UNP=range(NPOLS)
+POLS_COLOR=['kRed','kBlack','kBlue']
+POLS_NAME=['POS','NEG','UNP']
 
 ANADIR = os.environ['E1F_2PI_ANADIR2']
 TOP = "1:2:3:4"
 Q2WBNG = '1-2.000-2.400__24-1.300-1.900'
 SIMDIR = os.path.join(ANADIR,'simdir')
+
 SEQ_POLS_H5FILE = np.zeros((NSEQ,NPOLS),object)
-SEQ_POLS_H5FILE[ST:SF+1,POS:NEG+1]='%s/%s__%s__pol__sim.root'%(SIMDIR,TOP,Q2WBNG)
-SEQ_POLS_H5FILE[ST:SF+1,UNP:AVG]  ='%s/%s__%s__sim.root'%(SIMDIR,TOP,Q2WBNG)
-SEQ_POLS_H5FILE[ER:,POS:NEG+1]    ='%s/%s__%s__pol__exp.root'%(ANADIR,TOP,Q2WBNG)
-SEQ_POLS_H5FILE[ER:,UNP:AVG]      ='%s/%s__%s__exp.root'%(ANADIR,TOP,Q2WBNG)
+SEQ_POLS_H5FILE[ST:SF+1,POS:NEG+1]  ='%s/%s__%s__pol__sim.root'%(SIMDIR,TOP,Q2WBNG)
+SEQ_POLS_H5FILE[ST:SF+1,UNP:]       ='%s/%s__%s__sim.root'%(SIMDIR,TOP,Q2WBNG)
+SEQ_POLS_H5FILE[ER:,POS:NEG+1]      ='%s/%s__%s__pol__exp.root'%(ANADIR,TOP,Q2WBNG)
+SEQ_POLS_H5FILE[ER:,UNP:]           ='%s/%s__%s__exp.root'%(ANADIR,TOP,Q2WBNG)
 #print SEQ_POLS_H5FILE
 
 SEQ_POLS_H5=[
-	['hY5D/Varset1/hY5D_TH','hY5D/Varset1/hY5D_TH','hY5D/Varset1/hY5D_TH',''],
-	['hY5D/Varset1/hY5D_RECO','hY5D/Varset1/hY5D_RECO','hY5D/Varset1/hY5D_RECO',''],
-	['hY5D/Varset1/hY5D_ACC','hY5D/Varset1/hY5D_ACC','hY5D/Varset1/hY5D_ACC',''],
-	['hY5D/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR',''],
-	['hY5D/Varset1/hY5D_HOLE','hY5D/Varset1/hY5D_HOLE','hY5D/Varset1/hY5D_HOLE',''],
-	['hY5D/Varset1/hY5D_FULL','hY5D/Varset1/hY5D_FULL','hY5D/Varset1/hY5D_FULL',''],
-	['hY5D_POS/Varset1/hY5D_RECO',    'hY5D_NEG/Varset1/hY5D_RECO',    'hY5D/Varset1/hY5D_RECO',''],
-	['hY5D_POS/Varset1/hY5D_ACC_CORR','hY5D_NEG/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR',''],
-	['hY5D_POS/Varset1/hY5D_FULL',    'hY5D_NEG/Varset1/hY5D_FULL',    'hY5D/Varset1/hY5D_FULL','']
+	['hY5D/Varset1/hY5D_TH',      'hY5D/Varset1/hY5D_TH',      'hY5D/Varset1/hY5D_TH'],
+	['hY5D/Varset1/hY5D_RECO',    'hY5D/Varset1/hY5D_RECO',    'hY5D/Varset1/hY5D_RECO'],
+	['hY5D/Varset1/hY5D_ACC',     'hY5D/Varset1/hY5D_ACC',     'hY5D/Varset1/hY5D_ACC'],
+	['hY5D/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR'],
+	['hY5D/Varset1/hY5D_HOLE',    'hY5D/Varset1/hY5D_HOLE',    'hY5D/Varset1/hY5D_HOLE'],
+	['hY5D/Varset1/hY5D_FULL',    'hY5D/Varset1/hY5D_FULL',    'hY5D/Varset1/hY5D_FULL'],
+	['hY5D_POS/Varset1/hY5D_RECO',    'hY5D_NEG/Varset1/hY5D_RECO',    'hY5D/Varset1/hY5D_RECO'],
+	['hY5D_POS/Varset1/hY5D_ACC_CORR','hY5D_NEG/Varset1/hY5D_ACC_CORR','hY5D/Varset1/hY5D_ACC_CORR'],
+	['hY5D_POS/Varset1/hY5D_FULL',    'hY5D_NEG/Varset1/hY5D_FULL',    'hY5D/Varset1/hY5D_FULL']
 ]
 
 NPOBS=4
@@ -239,105 +241,38 @@ def makedf():
 	"""For a particular Q2W analysis range, starting at the level of h5s, put all analysis
 	objects into a DataFrame
 
-	output -- ana_store.h5
+	output -- DF5.h5
 	contains DataFrame d with analysis data represented by columns:
 	q2wbinnum -- q2wbin -- SEQ -- POL -- h5 -- h1{ij} -- h5{p} -- hR2_{p}^{ij}
 
 	"""
-	outfile = os.path.join(ANADIR,'ana_store.h5')
+	outfile = os.path.join(ANADIR,'DF5.h5')
 	store = pd.HDFStore(outfile)
 
-	# seql = [EC]
-	# poll = [POS,NEG,AVG]
+	norm = 50000*math.pi
 
 	d = pd.DataFrame()
 	ftemplate = root_open(SEQ_POLS_H5FILE[0][0])
 	keys = ftemplate.GetListOfKeys()
 
+	#1. First do the "looping part" of process of creating DF
 	q2wbinnum=0
 	dl_counter=0 #counter for number of dls (=Data-Lists, defined later) insereted into DF
 	for q2wdir in keys:
 		q2wbinnum+=1
 		for seq in range(0,NSEQ):
-			# The following indexing is required for reason of
-			# *practicality* and *ease-of-reading* the code
-			# 	- *ease-of-reading* code requires POLS
-			#	- *Practical* reason requires POBS,VARSETS,VARS since
-			#	  after obtain hR2(POLS=POS,NEG,UNP), hR2(POL=AVG) needs to be 
-			#	  calculated for all VARSETS,VARS and this is done in a seperate loop
-			h5={}#indexed by POLS
-			h1={}#indexed by POLS
-			h5p={}#indexed by POLS,POBS
-			hR2={}#indexed by POLS,POBS,VARSETS,VARS
-			norm = 50000*math.pi
 			for pol in range(0,NPOLS):#poll:
-				hists=[]
 				dl_counter+=1
-				if pol!=AVG:
-					f=root_open(SEQ_POLS_H5FILE[seq][pol])
-					h5[pol]=f.Get('%s/%s'%(q2wdir.GetName(),SEQ_POLS_H5[seq][pol]))
-					f.Close()
-					hists.append(h5[pol])
-					
-					#1. do 1D obs
-					for varset in range(0,NVARSETS):
-							if varset==1 or varset==2: continue
-							for var in range(0,NVARS):
-								h1[pol]=h5[pol].Projection(var)
-								hists.append(h1[pol])
-					#2. do Pol. Obs.			
-					for pob in range(0,NPOBS):
-						if   pob==A: continue
-						elif pob==B: h5p[(pol,pob)]=mythnt.MultiplyBy(h5[pol],'cphi')
-						elif pob==C: h5p[(pol,pob)]=mythnt.MultiplyBy(h5[pol],'c2phi')
-						elif pob==D:
-							if   pol==POS or pol==UNP:
-								h5p[(pol,pob)]=mythnt.MultiplyBy(h5[pol],'sphi')
-							elif pol==NEG:
-								h5p[(pol,pob)]=mythnt.MultiplyBy(h5[pol],'sphi',-1)
-						hists.append(h5p[(pol,pob)])	
-						for varset in range(0,NVARSETS):
-							if varset==1 or varset==2: continue
-							for var in range(0,NVARS):
-								hR2[(pol,pob,varset,var)]=h5p[(pol,pob)].Projection(var)
-								hR2[(pol,pob,varset,var)].Scale(1/norm)
-								hists.append(hR2[(pol,pob,varset,var)])
-				elif pol==AVG:
-					hists.append('') #blank version of h5[pol]
-					#1. do 1D obs
-					for varset in range(0,NVARSETS):
-							if varset==1 or varset==2: continue
-							for var in range(0,NVARS):
-								hists.append('') #blank version of h1_ij
-					#2. do Pol.Obs
-					for pob in range(0,NPOBS):
-						if pob==A: continue
-						hists.append('') #blank version of h5p[pol]
-						for varset in range(0,NVARSETS):
-							if varset==1 or varset==2: continue
-							for var in range(0,NVARS):
-								hR2[(AVG,pob,varset,var)]=hR2[(POS,pob,varset,var)].Clone("avg")
-								hR2[(AVG,pob,varset,var)].Add(hR2[(NEG,pob,varset,var)])
-								hR2[(AVG,pob,varset,var)].Scale(0.5)
-								if pob==D:
-									hR2[(AVG,pob,varset,var)].SetMinimum(-0.003)
-									hR2[(AVG,pob,varset,var)].SetMaximum(0.003)
-								hists.append(hR2[(AVG,pob,varset,var)])		
-				"""Create Data-List (dl) to be added to the DataFrame"""			
-				#dl = [q2wbinnum,q2wdir.GetName(),SEQ_NAME[seq],POLS_NAME[pol]]
-				dl = [q2wbinnum,q2wdir.GetName(),seq,pol]
-				dl+=hists
-				rindex=['q2wbinnum','q2wbin','SEQ','POL',
-				'h5', 'h1_1M1',   'h1_1M2',   'h1_1THETA',   'h1_1PHI',   'h1_1ALPHA',
-				'h5B','hR2_B_1M1','hR2_B_1M2','hR2_B_1THETA','hR2_B_1PHI','hR2_B_1ALPHA',
-				'h5C','hR2_C_1M1','hR2_C_1M2','hR2_C_1THETA','hR2_C_1PHI','hR2_C_1ALPHA',
-				'h5D','hR2_D_1M1','hR2_D_1M2','hR2_D_1THETA','hR2_D_1PHI','hR2_D_1ALPHA']
+				
+				f=root_open(SEQ_POLS_H5FILE[seq][pol])
+				h5=f.Get('%s/%s'%(q2wdir.GetName(),SEQ_POLS_H5[seq][pol]))
+				f.Close()
+									
+				#Create Data-List (dl) to be added to the DataFrame			
+				dl = [q2wbinnum,q2wdir.GetName(),seq,pol,h5]
+				rindex=['q2wbinnum','q2wbin','SEQ','POL','h5']
 				print 'len(dl)=',len(dl)
 				print 'len(rindex)=',len(rindex)
-				# print 'dl='
-				# print dl
-				# print 'rindex='
-				# print rindex
 				if not d:
 					data = pd.DataFrame({'s1':dl},index=rindex) # Data for 1st. Column 
 					d = d.append(data)
@@ -345,15 +280,24 @@ def makedf():
 					d['s%d'%dl_counter]=dl
 	
 	dt = d.transpose()
+	
+	#2. Now use semi-vectorized operation to fill up rest of the DF5
+	h5s = dt['h5']
+	h1s=[]
+	for i in range(len(h5s)):
+		h1s.append(h5s[i].Projection(M1))
+		#dt['h1_1M1']=dt['h5']
+	dt['h1_1M1']=h1s
+
 	store['d']=dt
-	#return dt					
+
 
 # MAIN part of the program
 #COSMETICS
 gStyle.SetOptStat(0);
 gStyle.SetOptFit(1111);
 #INPUT data
-infile = os.path.join(ANADIR,'ana_store.h5')
+infile = os.path.join(ANADIR,'DF5.h5')
 #OUTPUT data
 outdir_1D_root = os.path.join(ANADIR,'1D')
 outdir_root = os.path.join(ANADIR,'polobs.new')
@@ -373,23 +317,23 @@ d_grpd_q2wbin=d.groupby('q2wbin')
 nq2wbins = len(d_grpd_q2wbin)
 print 'nq2wbins=',nq2wbins
 
-for q2wbin in d_grpd_q2wbin.groups:
-		d_q2w=d_grpd_q2wbin.get_group(q2wbin)
-		outdir_q2w=os.path.join(outdir_root,q2wbin)
-		if not os.path.isdir(outdir_q2w):
-			os.makedirs(outdir_q2w)
+# for q2wbin in d_grpd_q2wbin.groups:
+# 		d_q2w=d_grpd_q2wbin.get_group(q2wbin)
+# 		outdir_q2w=os.path.join(outdir_root,q2wbin)
+# 		if not os.path.isdir(outdir_q2w):
+# 			os.makedirs(outdir_q2w)
 		
-		seq_pol_sell =[
-			(d_q2w['SEQ']==SF) & (d_q2w['POL']==UNP),
-			(d_q2w['SEQ']==EF) & (d_q2w['POL']==UNP),
-			(d_q2w['SEQ']==EF) & (d_q2w['POL']==POS),
-			(d_q2w['SEQ']==EF) & (d_q2w['POL']==NEG),
-			(d_q2w['SEQ']==EC) & (d_q2w['POL']==AVG) #dnp results
-		]
+# 		seq_pol_sell =[
+# 			(d_q2w['SEQ']==SF) & (d_q2w['POL']==UNP),
+# 			(d_q2w['SEQ']==EF) & (d_q2w['POL']==UNP),
+# 			(d_q2w['SEQ']==EF) & (d_q2w['POL']==POS),
+# 			(d_q2w['SEQ']==EF) & (d_q2w['POL']==NEG),
+# 			(d_q2w['SEQ']==EC) & (d_q2w['POL']==AVG) #dnp results
+# 		]
 		
 		#plotR2(seq_pol_sell)
-		plot1D()
+		#plot1D()
 
 #Now put all q2wbin/seq/R2^{ij}_{alpha} in a single pdf
 #makepdf()
-make1Dpdf()
+#make1Dpdf()
