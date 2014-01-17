f2421b00c219b3501e8853a3a7d4859fa36fb915
Arjun <arjuntvd@gmail.com>
2014-01-17 12:51:47 -0500

    updated plotMM() to use d2pi.root (instead of yield.root)

--- scripts/python/ana_tools.py
+++ scripts/python/ana_tools.py
@@ -7,48 +7,54 @@ from rootpy.interactive import wait
 
 import matplotlib.pyplot as plt
 
-DTYPS=2
-EXP,SIM = range(DTYPS)
-DTYPS_NAME=['exp','sim']
-
 def plotMM():
-	gStyle.SetOptFit(1111)
+	DTYPS=2
+	EXP,SIM = range(DTYPS)
+	DTYPS_NAME=['exp','sim']
+
+	ROOT.gStyle.SetOptFit(1111)
 	NMM=4
 	hmms=[[],[]]
 	hmm2s=[[],[]]
 
 	f=[]
-	f.append(root_open('/e1f.2pi.anadir2/yield.root'))
-	f.append(root_open('/e1f.2pi.anadir2/simdir/yield.root'))
-	#f = TF1("gaus","gaus",)
+	# f.append(root_open('/datadir2/e1f/ana-2pi/exp/q2w1/d2pi.root'))
+	# f.append(root_open('/e1f.2pi.anadir2/simdir/yield.root'))
+	f.append(ROOT.TFile("/datadir2/e1f/ana-2pi/exp/q2w1/d2pi.root"))
+	f.append(ROOT.TFile("/e1f.2pi.anadir2/simdir/yield.root"))
+	
 
 	for idt in range(DTYPS):
-		hmms[idt].append(f[idt].top.hmmppippimVw.ProjectionY('hmmppippim_%s'%DTYPS_NAME[idt]))
-		hmms[idt].append(f[idt].top.hmmppipVw.ProjectionY('hmmppip_%s'%DTYPS_NAME[idt]))
-		hmms[idt].append(f[idt].top.hmmppimVw.ProjectionY('hmmppim_%s'%DTYPS_NAME[idt]))
-		hmms[idt].append(f[idt].top.hmmpippimVw.ProjectionY('hmmpippim_%s'%DTYPS_NAME[idt]))
-		hmm2s[idt].append(f[idt].top.hmm2ppippimVw.ProjectionY('hmm2ppippim_%s'%DTYPS_NAME[idt]))
-		hmm2s[idt].append(f[idt].top.hmm2ppipVw.ProjectionY('hmm2ppip_%s'%DTYPS_NAME[idt]))
-		hmm2s[idt].append(f[idt].top.hmm2ppimVw.ProjectionY('hmm2ppim_%s'%DTYPS_NAME[idt]))
-		hmm2s[idt].append(f[idt].top.hmm2pippimVw.ProjectionY('hmm2pippim_%s'%DTYPS_NAME[idt]))
+		# hmms[idt].append(f[idt].top.hmmppippimVw.ProjectionY('hmmppippim_%s'%DTYPS_NAME[idt]))
+		# hmms[idt].append(f[idt].top.hmmppipVw.ProjectionY('hmmppip_%s'%DTYPS_NAME[idt]))
+		# hmms[idt].append(f[idt].top.hmmppimVw.ProjectionY('hmmppim_%s'%DTYPS_NAME[idt]))
+		# hmms[idt].append(f[idt].top.hmmpippimVw.ProjectionY('hmmpippim_%s'%DTYPS_NAME[idt]))
+		# hmm2s[idt].append(f[idt].top.hmm2ppippimVw.ProjectionY('hmm2ppippim_%s'%DTYPS_NAME[idt]))
+		# hmm2s[idt].append(f[idt].top.hmm2ppipVw.ProjectionY('hmm2ppip_%s'%DTYPS_NAME[idt]))
+		# hmm2s[idt].append(f[idt].top.hmm2ppimVw.ProjectionY('hmm2ppim_%s'%DTYPS_NAME[idt]))
+		# hmm2s[idt].append(f[idt].top.hmm2pippimVw.ProjectionY('hmm2pippim_%s'%DTYPS_NAME[idt]))
 		
-		# hmms[idt].append(f[idt].Get("/top/hmmppippimVw").ProjectionY('hmmppippim_%s'%DTYPS_NAME[idt]))
-		# hmms[idt].append(f[idt].Get("/top/hmmppimVw").ProjectionY('hmmppim_%s'%DTYPS_NAME[idt]))
-		# hmms[idt].append(f[idt].Get("/top/hmmppipVw").ProjectionY('hmmppip_%s'%DTYPS_NAME[idt]))
-		# hmms[idt].append(f[idt].Get("/top/hmmpippimVw").ProjectionY('hmmpippim_%s'%DTYPS_NAME[idt]))
+		hmms[idt].append(f[idt].Get("/top/hmmppippimVw").ProjectionY('hmmppippim_%s'%DTYPS_NAME[idt]))
+		hmms[idt].append(f[idt].Get("/top/hmmppimVw").ProjectionY('hmmppim_%s'%DTYPS_NAME[idt]))
+		hmms[idt].append(f[idt].Get("/top/hmmppipVw").ProjectionY('hmmppip_%s'%DTYPS_NAME[idt]))
+		hmms[idt].append(f[idt].Get("/top/hmmpippimVw").ProjectionY('hmmpippim_%s'%DTYPS_NAME[idt]))
+		hmm2s[idt].append(f[idt].Get("/top/hmm2ppippimVw").ProjectionY('hmm2ppippim_%s'%DTYPS_NAME[idt]))
+		hmm2s[idt].append(f[idt].Get("/top/hmm2ppimVw").ProjectionY('hmm2ppim_%s'%DTYPS_NAME[idt]))
+		hmm2s[idt].append(f[idt].Get("/top/hmm2ppipVw").ProjectionY('hmm2ppip_%s'%DTYPS_NAME[idt]))
+		hmm2s[idt].append(f[idt].Get("/top/hmm2pippimVw").ProjectionY('hmm2pippim_%s'%DTYPS_NAME[idt]))
 
-	cmm = TCanvas("mm","mm")
+	cmm = ROOT.TCanvas("mm","mm")
 	cmm.Divide(2,2)
 	fsimc=[]
 	for imm in range(NMM):
 		pad = cmm.cd(imm+1)
 		# hmms[EXP][imm].SetLineColor(gROOT.ProcessLine("kBlue"))
 		# hmms[EXP][imm].DrawNormalized("",10000)
-		hmms[SIM][imm].SetLineColor(gROOT.ProcessLine("kRed"))
-		hmms[SIM][imm].SetMarkerColor(gROOT.ProcessLine("kRed"))
+		hmms[SIM][imm].SetLineColor(ROOT.gROOT.ProcessLine("kRed"))
+		hmms[SIM][imm].SetMarkerColor(ROOT.gROOT.ProcessLine("kRed"))
 		hsim = hmms[SIM][imm].DrawNormalized("",10000)
-		hmms[EXP][imm].SetLineColor(gROOT.ProcessLine("kBlue"))
-		hmms[EXP][imm].SetMarkerColor(gROOT.ProcessLine("kBlue"))
+		hmms[EXP][imm].SetLineColor(ROOT.gROOT.ProcessLine("kBlue"))
+		hmms[EXP][imm].SetMarkerColor(ROOT.gROOT.ProcessLine("kBlue"))
 		hexp=hmms[EXP][imm].DrawNormalized("sames",10000)
 
 		if imm==1 or imm==2:
@@ -59,27 +65,27 @@ def plotMM():
 			hexp.Fit("gaus","","",0.9,0.96)
 		if imm!=0:
 			fsim = hsim.GetFunction("gaus")
-			fsim.SetLineColor(gROOT.ProcessLine("kBlack"))
+			fsim.SetLineColor(ROOT.gROOT.ProcessLine("kBlack"))
 			fexp = hexp.GetFunction("gaus")
-			fexp.SetLineColor(gROOT.ProcessLine("kBlack"))
+			fexp.SetLineColor(ROOT.gROOT.ProcessLine("kBlack"))
 			pad.Update()
-			fsimc.append(TF1(fsim))
-			fsimc[imm-1].SetLineColor(gROOT.ProcessLine("kGreen"))
+			fsimc.append(ROOT.TF1(fsim))
+			fsimc[imm-1].SetLineColor(ROOT.gROOT.ProcessLine("kGreen"))
 			fsimc[imm-1].SetRange(hmms[SIM][imm].GetXaxis().GetXmin(),hmms[SIM][imm].GetXaxis().GetXmax())
 			fsimc[imm-1].Draw("same")
 			#pad.Update()
 
-	cmm2 = TCanvas("mm2","mm2")
+	cmm2 = ROOT.TCanvas("mm2","mm2")
 	cmm2.Divide(2,2)
 	for imm in range(NMM):
 		pad = cmm2.cd(imm+1)
 		# hmms[EXP][imm].SetLineColor(gROOT.ProcessLine("kBlue"))
 		# hmms[EXP][imm].DrawNormalized("",10000)
-		hmm2s[SIM][imm].SetLineColor(gROOT.ProcessLine("kRed"))
-		hmm2s[SIM][imm].SetMarkerColor(gROOT.ProcessLine("kRed"))
+		hmm2s[SIM][imm].SetLineColor(ROOT.gROOT.ProcessLine("kRed"))
+		hmm2s[SIM][imm].SetMarkerColor(ROOT.gROOT.ProcessLine("kRed"))
 		hsim = hmm2s[SIM][imm].DrawNormalized("",10000)
-		hmm2s[EXP][imm].SetLineColor(gROOT.ProcessLine("kBlue"))
-		hmm2s[EXP][imm].SetMarkerColor(gROOT.ProcessLine("kBlue"))
+		hmm2s[EXP][imm].SetLineColor(ROOT.gROOT.ProcessLine("kBlue"))
+		hmm2s[EXP][imm].SetMarkerColor(ROOT.gROOT.ProcessLine("kBlue"))
 		hexp=hmm2s[EXP][imm].DrawNormalized("sames",10000)
 
 		if imm==1 or imm==2:
@@ -89,11 +95,11 @@ def plotMM():
 			hsim.Fit("gaus","","",0.9,1.0)
 			hexp.Fit("gaus","","",0.9,0.96)
 		if imm!=0:
-			hsim.GetFunction("gaus").SetLineColor(gROOT.ProcessLine("kBlack"))
-			hexp.GetFunction("gaus").SetLineColor(gROOT.ProcessLine("kBlack"))
+			hsim.GetFunction("gaus").SetLineColor(ROOT.gROOT.ProcessLine("kBlack"))
+			hexp.GetFunction("gaus").SetLineColor(ROOT.gROOT.ProcessLine("kBlack"))
 			pad.Update()
 
-	if not gROOT.IsBatch():
+	if not ROOT.gROOT.IsBatch():
 		plt.show()
 		# wait for you to close the ROOT canvas before exiting
 		wait(True)
