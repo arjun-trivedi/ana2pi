b4203a271c771d12f7b5a780efcd6b7b648d288e
Arjun <arjuntvd@gmail.com>
2014-02-07 13:45:51 -0500

    updates

--- scripts/python/nb-study_E1F_Simulation.py
+++ scripts/python/nb-study_E1F_Simulation.py
@@ -11,8 +11,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = '/data/trivedia/e1f/simulation_2pi/setup_sim_CentOS6/gpppars/try1/q2w2_gpptest_14_011914/recon/d2pi.root'
-arr = root2array(f,'d2piR/tR',start=1,stop=5)#,start=1,stop=5)#start=1,stop=2)
+import os
+#f = '/data/trivedia/e1f/simulation_2pi/setup_sim_CentOS6/gpppars/try1/q2w2_gpptest_14_011914/recon/d2pi.root'
+ddir=os.environ['SETUPSIMCENTOS6_DATADIR']
+f = os.path.join(ddir,'d2pi.root')
+arr = root2array(f,'d2piTR/R/tR',start=1,stop=5)#,start=1,stop=5)#start=1,stop=2)
 df = pd.DataFrame(arr)
 print df
 
