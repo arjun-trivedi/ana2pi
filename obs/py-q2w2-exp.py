78514b8abdec7b67ba4d34cff9c63923c8362fe0
Arjun <arjuntvd@gmail.com>
2014-07-06 13:58:44 -0400

    1st time addition

---
+++ obs/py-q2w2-exp.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from proc_yields import ProcYields
+py={}
+py['exp']=ProcYields('q2w2','exp')
+py['exp'].proc()
---
+++ obs/py-q2w2-sim.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from proc_yields import ProcYields
+py={}
+py['sim']=ProcYields('q2w2','sim')
+py['sim'].proc()
