c61faaf6c1406dc3912ec73d14758108528efed2
Arjun <arjuntvd@gmail.com>
2014-07-22 09:21:39 -0400

    updates

--- obs/disp_yields.py
+++ obs/disp_yields.py
@@ -19,93 +19,111 @@ class DispYields:
 		c=ROOT.TCanvas()
 		c.Divide(3,3)
 		pad=c.cd(1)
-		hexp=h_dpp['EXP'][0].DrawNormalized("",1000)
-		hsim=h_dpp['SIM'][0].DrawNormalized("sames",1000)
+		hexp=h_dpp['EXP','F'][0].DrawNormalized("",1000)
+		hsim=h_dpp['SIM','F'][0].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dpp['EXP','H'][0].DrawNormalized("sames",1000)
+		h_dpp['SIM','T'][0].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(2)
-		hexp=h_rho['EXP'][0].DrawNormalized("",1000)
-		hsim=h_rho['SIM'][0].DrawNormalized("sames",1000)
+		hexp=h_rho['EXP','F'][0].DrawNormalized("",1000)
+		hsim=h_rho['SIM','F'][0].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_rho['EXP','H'][0].DrawNormalized("sames",1000)
+		h_rho['SIM','T'][0].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(3)
-		hexp=h_dzr['EXP'][0].DrawNormalized("",1000)
-		hsim=h_dzr['SIM'][0].DrawNormalized("sames",1000)
+		hexp=h_dzr['EXP','F'][0].DrawNormalized("",1000)
+		hsim=h_dzr['SIM','F'][0].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dzr['EXP','H'][0].DrawNormalized("sames",1000)
+		h_dzr['SIM','T'][0].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(4)
-		hexp=h_dpp['EXP'][1].DrawNormalized("",1000)
-		hsim=h_dpp['SIM'][1].DrawNormalized("sames",1000)
+		hexp=h_dpp['EXP','F'][1].DrawNormalized("",1000)
+		hsim=h_dpp['SIM','F'][1].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dpp['EXP','H'][1].DrawNormalized("sames",1000)
+		h_dpp['SIM','T'][1].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(5)
-		hexp=h_rho['EXP'][1].DrawNormalized("",1000)
-		hsim=h_rho['SIM'][1].DrawNormalized("sames",1000)
+		hexp=h_rho['EXP','F'][1].DrawNormalized("",1000)
+		hsim=h_rho['SIM','F'][1].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_rho['EXP','H'][1].DrawNormalized("sames",1000)
+		h_rho['SIM','T'][1].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(6)
-		hexp=h_dzr['EXP'][1].DrawNormalized("",1000)
-		hsim=h_dzr['SIM'][1].DrawNormalized("sames",1000)
+		hexp=h_dzr['EXP','F'][1].DrawNormalized("",1000)
+		hsim=h_dzr['SIM','F'][1].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dzr['EXP','H'][1].DrawNormalized("sames",1000)
+		h_dzr['SIM','T'][1].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(7)
-		hexp=h_dpp['EXP'][2].DrawNormalized("",1000)
-		hsim=h_dpp['SIM'][2].DrawNormalized("sames",1000)
+		hexp=h_dpp['EXP','F'][2].DrawNormalized("",1000)
+		hsim=h_dpp['SIM','F'][2].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dpp['EXP','H'][2].DrawNormalized("sames",1000)
+		h_dpp['SIM','T'][2].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(8)
-		hexp=h_rho['EXP'][2].DrawNormalized("",1000)
-		hsim=h_rho['SIM'][2].DrawNormalized("sames",1000)
+		hexp=h_rho['EXP','F'][2].DrawNormalized("",1000)
+		hsim=h_rho['SIM','F'][2].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_rho['EXP','H'][2].DrawNormalized("sames",1000)
+		h_rho['SIM','T'][2].DrawNormalized("sames",1000)
 		pad.Update()
 
 		pad=c.cd(9)
-		hexp=h_dzr['EXP'][2].DrawNormalized("",1000)
-		hsim=h_dzr['SIM'][2].DrawNormalized("sames",1000)
+		hexp=h_dzr['EXP','F'][2].DrawNormalized("",1000)
+		hsim=h_dzr['SIM','F'][2].DrawNormalized("sames",1000)
 		maximum=hexp.GetMaximum()
 		if hsim.GetMaximum()>hexp.GetMaximum():
 			maximum=hsim.GetMaximum()
 		hexp.SetMaximum(maximum+10)
 		hsim.SetMaximum(maximum+10)
+		h_dzr['EXP','H'][2].DrawNormalized("sames",1000)
+		h_dzr['SIM','T'][2].DrawNormalized("sames",1000)
 		pad.Update()
 		
 		c.SaveAs("%s/c%s.png"%(self.ANADIR,q2wbin))
@@ -129,22 +147,36 @@ class DispYields:
 		for q2w in q2ws:
 			h_dpp,h_rho,h_dzr=OrderedDict(),OrderedDict(),OrderedDict()
 			for dtyp in ['EXP','SIM']:
-				if dtyp=='EXP':
-					f=self.FEXP
-					col=ROOT.gROOT.ProcessLine("kBlue")
-				if dtyp=='SIM':
-					f=self.FSIM
-					col=ROOT.gROOT.ProcessLine("kRed")
-				h_dpp[dtyp]=[f.Get("%s/%s"%(q2w,"VST1/F/h_M1")),f.Get("%s/%s"%(q2w,"VST1/F/h_THETA")),
-							 f.Get("%s/%s"%(q2w,"VST1/F/h_ALPHA"))]
-				h_rho[dtyp]=[f.Get("%s/%s"%(q2w,"VST2/F/h_M2")),f.Get("%s/%s"%(q2w,"VST2/F/h_THETA")),
-							 f.Get("%s/%s"%(q2w,"VST2/F/h_ALPHA"))]
-				h_dzr[dtyp]=[f.Get("%s/%s"%(q2w,"VST3/F/h_M2")),f.Get("%s/%s"%(q2w,"VST3/F/h_THETA")),
-							 f.Get("%s/%s"%(q2w,"VST3/F/h_ALPHA"))]
-				for hists in [h_dpp[dtyp],h_rho[dtyp],h_dzr[dtyp]]:
-					#print hists
-					for h in hists:
-						#print h
-						h.SetLineColor(col)
-						h.SetMarkerColor(col)
+				for seq in ['T','H','F']:
+					if dtyp=='EXP' and seq=='T': continue
+					if dtyp=='SIM' and seq=='H': continue
+
+					if dtyp=='EXP':
+						f=self.FEXP
+						if seq=='F':
+							col=ROOT.gROOT.ProcessLine("kBlue")
+						if seq=='H':
+							col=ROOT.gROOT.ProcessLine("kBlack")
+					if dtyp=='SIM':
+						f=self.FSIM
+						if seq=='F':
+							col=ROOT.gROOT.ProcessLine("kRed")
+						if seq=='T':
+							col=ROOT.gROOT.ProcessLine("kGreen")
+							mst=ROOT.gROOT.ProcessLine("kPlus")
+
+					h_dpp[dtyp,seq]=[f.Get("%s/VST1/%s/h_M1"%(q2w,seq)),f.Get("%s/VST1/%s/h_THETA"%(q2w,seq)),
+								 	f.Get("%s/VST1/%s/h_ALPHA"%(q2w,seq))]
+					h_rho[dtyp,seq]=[f.Get("%s/VST2/%s/h_M2"%(q2w,seq)),f.Get("%s/VST2/%s/h_THETA"%(q2w,seq)),
+								 	f.Get("%s/VST2/%s/h_ALPHA"%(q2w,seq))]
+					h_dzr[dtyp,seq]=[f.Get("%s/VST3/%s/h_M2"%(q2w,seq)),f.Get("%s/VST3/%s/h_THETA"%(q2w,seq)),
+								 	f.Get("%s/VST3/%s/h_ALPHA"%(q2w,seq))]
+					for hists in [h_dpp[dtyp,seq],h_rho[dtyp,seq],h_dzr[dtyp,seq]]:
+						#print hists
+						for h in hists:
+							#print h
+							h.SetLineColor(col)
+							h.SetMarkerColor(col)
+							if dtyp=='SIM' and seq=='T':
+								 h.SetMarkerStyle(mst)
 			self.plot_obs_1D(q2w,h_dpp,h_rho,h_dzr)
