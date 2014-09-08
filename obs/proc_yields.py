a2e8238e029571c17f7b108316e384d321670f2d
Arjun <arjuntvd@gmail.com>
2014-09-08 16:30:49 -0400

    updates

--- obs/proc_yields.py
+++ obs/proc_yields.py
@@ -33,7 +33,7 @@ H5_DIM=OrderedDict([('M1',0),('M2',1),('THETA',2),('PHI',3),('ALPHA',4)])
 VARS=['M1','M2','THETA','PHI','ALPHA']
 
 class ProcYields:
-	def __init__(self,q2w,dtyp,sim_num='siml',tops=[1,2,3,4],vsts=[1,2,3]):
+	def __init__(self,dtyp,sim_num='siml',tops=[1,2,3,4],vsts=[1,2,3]):
 		self.EXP,self.SIM=False,False
 		if dtyp=='sim':self.SIM=True
 		if dtyp=='exp':self.EXP=True
@@ -45,154 +45,139 @@ class ProcYields:
 		self.TOPS=tops 
 		self.VSTS=vsts 
 
-		#! Set up q2w bng
-		self.Q2W=q2w
-		self.Q2BNG,self.WBNG=None,None
-		q2w_num=int(self.Q2W.split('q2w')[1])
-		try:
-			self.Q2BNG,self.WBNG=atlib.init_q2wbng2(q2w_bng.Q2W_BNG[q2w_num-1])
-		except IndexError:
-			print "IndexError while setting q2w bng. Exiting"
-		except:
-			print "The following exception occured. Exiting."
-			raise
-    		#sys.exit()
-    	#! DNP=13 bng
-		# if self.Q2W=='q2w2':
-		# 	q2w_bng=[[2.0,2.5,0.5],[1.3,1.9,0.025]]
-		# 	self.Q2BNG,self.WBNG=atlib.init_q2wbng2(q2w_bng)
-		#! new bng
-		# q2w_bng=None
-		# if self.Q2W=='q2w1':
-		# 	q2w_bng=[[1.2,1.6,0.4],[1.3,1.7,0.025]]
-		# elif self.Q2W=='q2w2':
-		# 	q2w_bng=[[1.2,1.6,0.4],[1.7,2.0,0.025]]
-		# elif self.Q2W=='q2w3':
-		# 	q2w_bng=[[1.2,1.6,0.4],[2.0,2.2,0.025]]
-		# elif self.Q2W=='q2w4':
-		# 	q2w_bng=[[1.2,1.6,0.4],[2.2,2.4,0.025]]
-		# else:
-		# 	sys.exit("Exiting. %s binning not recognized"%self.Q2W)
-
-		# self.Q2BNG,self.WBNG=atlib.init_q2wbng2(q2w_bng)
-
-		# #! Test Q2WBNG, WBNG
-		# print "Q2 bins=",Q2BNG['BINS']
-		# print "W bins=",WBNG['BINS']
-		# for i in range(self.Q2BNG['NBINS']):
-		#     print "Q2 bin:",i+1
-		#     print self.Q2BNG['BINS_LE'][i]
-		#     print self.Q2BNG['BINS_UE'][i]
-		#     print self.Q2BNG['BINW']
-		# for i in range(self.WBNG['NBINS']):
-		#     print "W bin:",i+1
-		#     print self.WBNG['BINS_LE'][i]
-		#     print self.WBNG['BINS_UE'][i]
-		#     print self.WBNG['BINW']
-
 		if self.EXP:
-			self.DATADIR=os.environ['OBS_DATADIR_EXP']
+			self.DATADIR='$HOME/ongoing/mem_test/exp/new-h8-bng'
+			#!self.DATADIR=os.environ['OBS_DATADIR_EXP']
 			self.FIN=ROOT.TFile(os.path.join(self.DATADIR,'d2pi.root'))
-			self.ANADIR=os.path.join(os.environ['OBS_DIR'],self.SIM_NUM,self.Q2W)
-			if not os.path.exists(self.ANADIR):
+			self.OUTDIR=self.DATADIR
+			#!self.OUTDIR=os.path.join(os.environ['OBS_DIR'],self.SIM_NUM,self.Q2W)
+			if not os.path.exists(self.OUTDIR):
 				#! This path should already exist when making yield_sim
-				sys.exit("Path %s does not exist. Exiting."%self.ANADIR)
-				#os.makedirs(self.ANADIR)
-			self.FIN_SIMYIELD=ROOT.TFile(os.path.join(self.ANADIR,"yield_sim.root"))
-			self.FOUT=ROOT.TFile(os.path.join(self.ANADIR,"yield_exp.root"),"RECREATE")
-			print "DATADIR=%s\nANADIR=%s\nFIN=%s\nFIN_SIMYIELD=%s\nFOUT=%s"%(self.DATADIR,self.ANADIR,self.FIN.GetName(),self.FIN_SIMYIELD.GetName(),self.FOUT.GetName())
+				sys.exit("Path %s does not exist. Exiting."%self.OUTDIR)
+				#os.makedirs(self.OUTDIR)
+			self.FIN_SIMYIELD=ROOT.TFile(os.path.join(self.OUTDIR,"yield_sim.root"))
+			self.FOUT=ROOT.TFile(os.path.join(self.OUTDIR,"yield_exp.root"),"RECREATE")
+			print "DATADIR=%s\nOUTDIR=%s\nFIN=%s\nFIN_SIMYIELD=%s\nFOUT=%s"%(self.DATADIR,self.OUTDIR,self.FIN.GetName(),self.FIN_SIMYIELD.GetName(),self.FOUT.GetName())
 		if self.SIM:
-			self.DATADIR=os.path.join(os.environ['OBS_DATADIR_SIM'],self.SIM_NUM,self.Q2W)
+			self.DATADIR='$HOME/ongoing/mem_test/sim/new-h8-bng'
+			#self.DATADIR=os.path.join(os.environ['OBS_DATADIR_SIM'],self.SIM_NUM,self.Q2W)
 			self.FIN=ROOT.TFile(os.path.join(self.DATADIR,'d2pi.root'))
-			self.ANADIR=os.path.join(os.environ['OBS_DIR'],self.SIM_NUM,self.Q2W)
-			if not os.path.exists(self.ANADIR):
-				os.makedirs(self.ANADIR)
-			self.FOUT=ROOT.TFile(os.path.join(self.ANADIR,"yield_sim.root"),"RECREATE")
-			print "DATADIR=%s\nANADIR=%s\nFIN=%s\nFOUT=%s"%(self.DATADIR,self.ANADIR,self.FIN.GetName(),self.FOUT.GetName())
+			self.OUTDIR=self.DATADIR
+			#!self.OUTDIR=os.path.join(os.environ['OBS_DIR'],self.SIM_NUM,self.Q2W)
+			if not os.path.exists(self.OUTDIR):
+				os.makedirs(self.OUTDIR)
+			self.FOUT=ROOT.TFile(os.path.join(self.OUTDIR,"yield_sim.root"),"RECREATE")
+			print "DATADIR=%s\nOUTDIR=%s\nFIN=%s\nFOUT=%s"%(self.DATADIR,self.OUTDIR,self.FIN.GetName(),self.FOUT.GetName())
 
 		self.wmax=None #Used for setM1M2axisrange()
 	
 	def proc(self):
-		#! Fetch h8{}
-		#h8=OrderedDict()
-		h8=self.get_h8()
+		#! Loop over Crw-W bins
+		for iw in range(q2w_bng.NBINS_WCRS):
+			#! Get h8
+			h8=self.get_h8(iw)
+			#! Debug h8
+			for k in h8:
+				print k,h8[k].GetEntries()
+			#! Set up Q2,W bng
+			self.Q2BNG,self.WBNG=None,None
+			try:
+				self.Q2BNG,self.WBNG=atlib.init_q2wbng2(q2w_bng.Q2W_BNG[iw])
+			except IndexError:
+				print "IndexError while setting q2w bng. Exiting"
+			except:
+				print "The following exception occured. Exiting."
+				raise
 
-		# #! Debug h8{}
-		# for k in h8:
-		#     print k,h8[k].GetEntries()   
-  
-		#! Loop over [Q2BNG,WBNG],VSTS,SEQ, and project: h8->h5->h1
-		for i in range(self.Q2BNG['NBINS']):
-			for j in range(self.WBNG['NBINS']):
-				#if j>4: break
-				q2wbin="%0.1f-%0.1f_%0.3f-%0.3f"%(self.Q2BNG['BINS_LE'][i],self.Q2BNG['BINS_UE'][i],self.WBNG['BINS_LE'][j],self.WBNG['BINS_UE'][j])
-				q2wbindir=self.FOUT.mkdir(q2wbin)
-				q2wbintitle="[%0.1f,%0.1f)_[%0.3f,%0.3f)"%(self.Q2BNG['BINS_LE'][i],self.Q2BNG['BINS_UE'][i],self.WBNG['BINS_LE'][j],self.WBNG['BINS_UE'][j])
-				self.wmax=self.WBNG['BINS_UE'][j]
-				#hq2w,h5,h1=OrderedDict(),OrderedDict(),OrderedDict()
-				# self.hq2w.clear()
-				# self.h5.clear()
-				# self.h1.clear()
-				for vst in self.VSTS:
-					vst_name='VST%d'%vst
-					vstdir=q2wbindir.mkdir(vst_name)           
-					print "*** Processing %s,%s ***"%(q2wbin,vst_name)
-					#! First, for h8-ST/SR/ER, set appropriate ranges for HEL,Q2&W
-					print "*** h8 range setting for HEL,Q2 & W dimensions ... ***"
-					if self.EXP:seq_h8=['R']
-					if self.SIM:seq_h8=['T','R']
-					for seq in seq_h8:
-						#!-- HEL: include all helicities
-						h8[vst_name,seq].GetAxis(H8_DIM['HEL']).SetRange()
-						#!-- Q2
-						q2bin_le=h8[vst_name,seq].GetAxis(H8_DIM['Q2']).FindBin(self.Q2BNG['BINS_LE'][i]+self.Q2BNG['BINW']/2)
-						q2bin_ue=h8[vst_name,seq].GetAxis(H8_DIM['Q2']).FindBin(self.Q2BNG['BINS_UE'][i]-self.Q2BNG['BINW']/2)
-						h8[vst_name,seq].GetAxis(H8_DIM['Q2']).SetRange(q2bin_le,q2bin_ue)
-						#!-- W
-						wbin_le=h8[vst_name,seq].GetAxis(H8_DIM['W']).FindBin(self.WBNG['BINS_LE'][j]+self.WBNG['BINW']/2)
-						wbin_ue=h8[vst_name,seq].GetAxis(H8_DIM['W']).FindBin(self.WBNG['BINS_UE'][j]-self.WBNG['BINW']/2)
-						h8[vst_name,seq].GetAxis(H8_DIM['W']).SetRange(wbin_le,wbin_ue)
-						print "For h8(%s,%s),finished setting range for Q2-,W-bin = %s ***"%(vst_name,seq,q2wbintitle)
-						#! Project out hq2w & save (to FOUT & OS)
-						hq2w=h8[vst_name,seq].Projection(H8_DIM['Q2'],H8_DIM['W'],"E")
-						hq2w.SetName('h_q2Vw')
-						hq2w.SetTitle("%s_%s_%s_q2w"%(q2wbintitle,vst_name,seq))
-						# outdir=os.path.join(self.ANADIR,q2wbin,vst_name,seq)
-						# if not os.path.exists(outdir):
-						# 	os.makedirs(outdir)
-						#! cd into FOUT.q2wbindir.vstdir.seqdir
-						vstdir.mkdir(seq).cd()
-						hq2w.Write()
-						# cq2w=ROOT.TCanvas(hq2w.GetName(),hq2w.GetTitle())
-						# hq2w.Draw("colz")
-						# cq2w.SaveAs("%s/%s.png"%(outdir,cq2w.GetName()))
-					print "*** Done h8 range setting"
-					#! 1. h8->h5 
-					h5=self.proc_h5(h8,q2wbin,q2wbindir,q2wbintitle,vst_name,vstdir)
-					#! 2. h5->h1
-					self.proc_h1(h5,q2wbin,q2wbindir,q2wbintitle,vst_name,vstdir)
-		self.FOUT.Close()
+			#! Test Q2WBNG, WBNG
+			print "Q2 bins=",self.Q2BNG['BINS']
+			print "W bins=",self.WBNG['BINS']
+			# for i in range(self.Q2BNG['NBINS']):
+			# 	print "Q2 bin:",i+1
+			# 	print self.Q2BNG['BINS_LE'][i]
+			# 	print self.Q2BNG['BINS_UE'][i]
+			# 	print self.Q2BNG['BINW']
+			# for i in range(self.WBNG['NBINS']):
+			# 	print "W bin:",i+1
+			# 	print self.WBNG['BINS_LE'][i]
+			# 	print self.WBNG['BINS_UE'][i]
+			# 	print self.WBNG['BINW']
 
-	def get_h8(self):
+		# #! Loop over [Q2BNG,WBNG],VSTS,SEQ, and project: h8->h5->h1
+		# for i in range(self.Q2BNG['NBINS']):
+		# 	for j in range(self.WBNG['NBINS']):
+		# 		#if j>4: break
+		# 		q2wbin="%0.1f-%0.1f_%0.3f-%0.3f"%(self.Q2BNG['BINS_LE'][i],self.Q2BNG['BINS_UE'][i],self.WBNG['BINS_LE'][j],self.WBNG['BINS_UE'][j])
+		# 		q2wbindir=self.FOUT.mkdir(q2wbin)
+		# 		q2wbintitle="[%0.1f,%0.1f)_[%0.3f,%0.3f)"%(self.Q2BNG['BINS_LE'][i],self.Q2BNG['BINS_UE'][i],self.WBNG['BINS_LE'][j],self.WBNG['BINS_UE'][j])
+		# 		self.wmax=self.WBNG['BINS_UE'][j]
+		# 		#hq2w,h5,h1=OrderedDict(),OrderedDict(),OrderedDict()
+		# 		# self.hq2w.clear()
+		# 		# self.h5.clear()
+		# 		# self.h1.clear()
+		# 		for vst in self.VSTS:
+		# 			vst_name='VST%d'%vst
+		# 			vstdir=q2wbindir.mkdir(vst_name)           
+		# 			print "*** Processing %s,%s ***"%(q2wbin,vst_name)
+		# 			#! First, for h8-ST/SR/ER, set appropriate ranges for HEL,Q2&W
+		# 			print "*** h8 range setting for HEL,Q2 & W dimensions ... ***"
+		# 			if self.EXP:seq_h8=['R']
+		# 			if self.SIM:seq_h8=['T','R']
+		# 			for seq in seq_h8:
+		# 				#!-- HEL: include all helicities
+		# 				h8[vst_name,seq].GetAxis(H8_DIM['HEL']).SetRange()
+		# 				#!-- Q2
+		# 				q2bin_le=h8[vst_name,seq].GetAxis(H8_DIM['Q2']).FindBin(self.Q2BNG['BINS_LE'][i]+self.Q2BNG['BINW']/2)
+		# 				q2bin_ue=h8[vst_name,seq].GetAxis(H8_DIM['Q2']).FindBin(self.Q2BNG['BINS_UE'][i]-self.Q2BNG['BINW']/2)
+		# 				h8[vst_name,seq].GetAxis(H8_DIM['Q2']).SetRange(q2bin_le,q2bin_ue)
+		# 				#!-- W
+		# 				wbin_le=h8[vst_name,seq].GetAxis(H8_DIM['W']).FindBin(self.WBNG['BINS_LE'][j]+self.WBNG['BINW']/2)
+		# 				wbin_ue=h8[vst_name,seq].GetAxis(H8_DIM['W']).FindBin(self.WBNG['BINS_UE'][j]-self.WBNG['BINW']/2)
+		# 				h8[vst_name,seq].GetAxis(H8_DIM['W']).SetRange(wbin_le,wbin_ue)
+		# 				print "For h8(%s,%s),finished setting range for Q2-,W-bin = %s ***"%(vst_name,seq,q2wbintitle)
+		# 				#! Project out hq2w & save (to FOUT & OS)
+		# 				hq2w=h8[vst_name,seq].Projection(H8_DIM['Q2'],H8_DIM['W'],"E")
+		# 				hq2w.SetName('h_q2Vw')
+		# 				hq2w.SetTitle("%s_%s_%s_q2w"%(q2wbintitle,vst_name,seq))
+		# 				# outdir=os.path.join(self.OUTDIR,q2wbin,vst_name,seq)
+		# 				# if not os.path.exists(outdir):
+		# 				# 	os.makedirs(outdir)
+		# 				#! cd into FOUT.q2wbindir.vstdir.seqdir
+		# 				vstdir.mkdir(seq).cd()
+		# 				hq2w.Write()
+		# 				# cq2w=ROOT.TCanvas(hq2w.GetName(),hq2w.GetTitle())
+		# 				# hq2w.Draw("colz")
+		# 				# cq2w.SaveAs("%s/%s.png"%(outdir,cq2w.GetName()))
+		# 			print "*** Done h8 range setting"
+		# 			#! 1. h8->h5 
+		# 			h5=self.proc_h5(h8,q2wbin,q2wbindir,q2wbintitle,vst_name,vstdir)
+		# 			#! 2. h5->h1
+		# 			self.proc_h1(h5,q2wbin,q2wbindir,q2wbintitle,vst_name,vstdir)
+		# self.FOUT.Close()
+
+	def get_h8(self,iw):
+		"""
+		get h8[iw]
+		"""
 		h8=OrderedDict()
 		#! Fetch h8-ST/SR/ER directly from file (Add all tops)
 		for vst in self.VSTS:
 			vst_name='VST%d'%vst
 			#! Fetch h8-T
 			if self.SIM:
-				print "Going to get",'d2piT/yield_varset%d'%vst,"..."
-				h8[vst_name,'T']=self.FIN.Get('d2piT/yield_varset%d'%vst)
-				print "Done getting",'d2piT/yield_varset%d'%vst
+				print "Going to get",'d2piT/h8_%d_%d'%(iw+1,vst),"..."
+				h8[vst_name,'T']=self.FIN.Get('d2piT/h8_%d_%d'%(iw+1,vst))
+				print "Done getting",'d2piT/h8_%d_%d'%(iw+1,vst)
 			#! Fetch h8-R (Add all tops i.e. h8 = h8(t1)+h8(t2)+h8(t3)+h8(t4))
 			for itop,top in enumerate(self.TOPS):
 				if itop==0:
-					print "Going to get",'d2piR/top%d/yield_varset%d'%(top,vst),"..."
-					h8[vst_name,'R']=self.FIN.Get('d2piR/top%d/yield_varset%d'%(top,vst))
-					print "Done getting",'d2piR/top%d/yield_varset%d'%(top,vst)
+					print "Going to get",'d2piR/top%d/h8_%d_%d'%(top,iw+1,vst),"..."
+					h8[vst_name,'R']=self.FIN.Get('d2piR/top%d/h8_%d_%d'%(top,iw+1,vst))
+					print "Done getting",'d2piR/top%d/h8_%d_%d'%(top,iw+1,vst)
 				else:
-					print "Going to get and add to top1",'d2piR/top%d/yield_varset%d'%(top,vst),"..."
-					h8[vst_name,'R'].Add(self.FIN.Get('d2piR/top%d/yield_varset%d'%(top,vst)))
-					print "Done gettng and adding to top1",'d2piR/top%d/yield_varset%d'%(top,vst)
+					print "Going to get and add to top1",'d2piR/top%d/h8_%d_%d'%(top,iw+1,vst),"..."
+					h8[vst_name,'R'].Add(self.FIN.Get('d2piR/top%d/h8_%d_%d'%(top,iw+1,vst)))
+					print "Done gettng and adding to top1",'d2piR/top%d/h8_%d_%d'%(top,iw+1,vst)
 		return h8
 
 	def proc_h5(self,h8,q2wbin,q2wbindir,q2wbintitle,vst_name,vstdir):
@@ -273,7 +258,7 @@ class ProcYields:
 			seq_h1=['T','R','C','A','H','F']
 		for seq in seq_h1:
 			#! Create outdir in OS (I am currently saving 1D hist plots to view with gqview)
-			# outdir=os.path.join(self.ANADIR,q2wbin,vst_name,seq)
+			# outdir=os.path.join(self.OUTDIR,q2wbin,vst_name,seq)
 			# if not os.path.exists(outdir):
 			# 	os.makedirs(outdir)
 			#! cd into FOUT.q2wbindir.vstdir.seqdir(note seqdir already create in proc_h5)
