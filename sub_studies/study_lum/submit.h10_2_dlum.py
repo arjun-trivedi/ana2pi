5aba2dc7403df1cac096a851eae642aab6327cbd
Arjun Trivedi <atrivedi@ifarm1401.jlab.org>
2015-02-18 08:32:14 -0500

    updates

--- sub_studies/study_lum/submit.h10_2_dlum.py
+++ sub_studies/study_lum/submit.h10_2_dlum.py
@@ -2,7 +2,7 @@
 import glob,os
 from subprocess import Popen, PIPE, STDOUT
 
-SUBDIR="/tmp/e1f/2pi/exp/h10_2_dlum"#${BSUBDIR}/e1f/2pi/exp/h10_2_dlum
+SUBDIR=os.path.join(os.environ['BSUBDIR'],"e1f/2pi/exp/h10_2_dlum")
 if not os.path.exists(SUBDIR):
 	os.makedirs(SUBDIR)
 OUTDIR=os.environ['STUDY_LUM_DATADIR']
@@ -19,23 +19,31 @@ for h10lst in h10lstl:
 	if os.path.exists(JSUBFILE):
 		print "job.sub exists. Deleting it before creating new one."
 	 	os.remove(JSUBFILE)	
-	ff=open(JSUBFILE,"w")
-	ff.write("JOBNAME: e1f_h10_2_dlum_%s\n"%runnum)
-	ff.write("PROJECT: E1F\n")
-	ff.write("SINGLE_JOB: true\n")
-	ff.write("OS: centos65\n")
-	ff.write("TRACK: analysis\n")
-	ff.write("DISK_SPACE: 500 MB\n")
-	ff.write("MEMORY: 1000 MB\n")
-	ff.write("TIME: 2\n")
-	ff.write(" \n")
-	#echo "COMMAND: h10_2_h10skim-exp "$h10lst  >> ${JSUBFILE}
-	#echo "COMMAND: h10_2_h10skim-exp /tmp/atrivedi/$idtfr.lst"  >> ${JSUBFILE} 
-	ff.write("COMMAND: proc_h10 -i %s -t e1f:exp:2pi  -p lum -o %s/%d.root\n"%(h10lst,os.environ['STUDY_LUM_DATADIR'],runnum))
-        ff.write("OTHER_FILES: %s\n"%(os.path.join(os.environ['ANA2PI'],"proc_h10")))
-	#read h10lst and list as OTHER files from E1FD
+	fsub=open(JSUBFILE,"w")
+	fsub.write("JOBNAME: e1f_h10_2_dlum_%s\n"%runnum)
+	fsub.write("PROJECT: E1F\n")
+	fsub.write("SINGLE_JOB: true\n")
+	fsub.write("OS: centos65\n")
+	fsub.write("TRACK: analysis\n")
+	fsub.write("DISK_SPACE: 500 MB\n")
+	fsub.write("MEMORY: 1000 MB\n")
+	fsub.write("TIME: 5\n")
+	fsub.write(" \n")
+        #fsub.write("COMMAND: proc_h10 -i h10.lst -t e1f:exp:2pi  -p lum -o lum.root\n"%h10lst)
+        fsub.write("COMMAND: h10_2_dlum\n")
+	#! Read h10lst and append $E1FD to all files
+	#! and list as OTHER files for job (so that they can be got from Tape)
 	fh10lst=open(h10lst)
-	for rf in fh10lst.readlines():
-		ff.write("OTHER_FILES: %s/%s\n"%(os.environ['E1FD'],rf.strip()))
-	ff.write("INPUT_FILES: %s\n"%h10lst)
-	ff.write("INPUT_DATA: h10.lst\n")
+	for fh10 in fh10lst.readlines():
+		fsub.write("OTHER_FILES: %s/%s\n"%(os.environ['E1FD'],fh10.strip()))
+	fsub.write("INPUT_FILES: %s\n"%h10lst)
+	fsub.write("INPUT_DATA: h10.lst\n")
+        fsub.write("OUTPUT_DATA: lum.root\n")
+        fsub.write("OUTPUT_TEMPLATE: %s/%s.root\n"%(os.environ['STUDY_LUM_DATADIR'],runnum))
+
+        #! Now submit the job
+        cmd="jsub %s"%JSUBFILE
+        #p=Popen(cmd,shell=True,stdin=PIPE,stdout=PIPE,stderr=STDOUT,close_fds=True)
+	#stdout=p.stdout.read()
+    	#print stdout
+        print "Submitted job for runnum %d"%runnum 
