a73559b8b4fc3dc37115dae12b47930cf761e295
Arjun <arjuntvd@gmail.com>
2015-03-06 08:52:53 -0500

    updates

--- sub_studies/study_elastic/study_elastic_tools.py
+++ sub_studies/study_elastic/study_elastic_tools.py
@@ -16,7 +16,8 @@ def proc_yield():
 	
 	FOUT=ROOT.TFile(os.path.join(os.environ['OBSDIR_ELASTIC'],'yield.root'),"RECREATE")
 
-	#! Begin process of obtaining Raw and Acceptance corrected yiels
+	#! Begin process of obtaining Raw and Acceptance corrected yields
+
 	#! 1. Get all hN2s, calculate Acceptances and cast into h2 for easy viewing
 	hN2={}
 	for seq in ['ST','SR','SA','SC','ER','EC']:
@@ -27,49 +28,23 @@ def proc_yield():
 			hN2[seq]=fin.Get("delast/yield")
 			FOUT.mkdir(seq).cd()
 			hN2[seq].Write()
-			#! h2
-			h2=hN2[seq].Projection(H2_DIM['PHI'],H2_DIM['THETA'])
-			h2.SetName("h2")
-			h2.Write()
-			#! h1s
-			#h1={}
-			#for var in VARS:
-			#	h1[var]=hN2[seq].Projection(H2_DIM[var])
-			#	h1[var].SetName("h%s"%var)
-			#	h1[var].Write()
 		elif seq=='SA':#then calculate Acceptance from Simulation
 			hN2[seq]=hN2['SR'].Clone()
 			hN2[seq].Divide(hN2['ST'])
 			FOUT.mkdir(seq).cd()
 			hN2[seq].Write()
-			#! h2
-			h2=hN2[seq].Projection(H2_DIM['PHI'],H2_DIM['THETA'])
-                        h2.SetName("h2")
-                        h2.Write()
-			#! h1s
-                        #h1={}
-                        #for var in VARS:
-                        #        h1[var]=hN2[seq].Projection(H2_DIM[var])
-                        #        h1[var].SetName("h%s"%var)
-                        #        h1[var].Write()
 		elif seq=='SC' or seq=='EC':#then calculate SC and EC from SA
 			if   seq=='SC':hN2[seq]=hN2['SR'].Clone()
 			elif seq=='EC':hN2[seq]=hN2['ER'].Clone()
 			hN2[seq].Divide(hN2['SA'])
 			FOUT.mkdir(seq).cd()
                         hN2[seq].Write()
-			#! h2
-                        h2=hN2[seq].Projection(H2_DIM['PHI'],H2_DIM['THETA'])
-                        h2.SetName("h2")
-                        h2.Write()
-			#! h1s
-                        #h1={}
-                        #for var in VARS:
-                        #        h1[var]=hN2[seq].Projection(H2_DIM[var])
-                        #        h1[var].SetName("h%s"%var)
-                        #        h1[var].Write()
-		
-	#! 2. Now make projection on to THETA and PHI: "Direct" and on to THETA for PHI_PROJ_BINS
+		#! h2
+                h2=hN2[seq].Projection(H2_DIM['PHI'],H2_DIM['THETA'])
+                h2.SetName("h2")
+                h2.Write()	
+
+	#! 2. Now make projection on to THETA and PHI: Direct and on to THETA for PHI_PROJ_BINS
 	for seq in ['ST','SR','SA','SC','ER','EC']:
 		#! Direct projection on to THETA and PHI 
 		seqdir=FOUT.GetDirectory(seq)
@@ -98,17 +73,17 @@ def proc_yield():
 				hTHETA_2=hN2[seq].Projection(H2_DIM['THETA'])	
 				hTHETA=hTHETA_1.Clone()
 				hTHETA.Add(hTHETA_2)
+				hTHETA.SetName("hTHETA")
 				hTHETA.Write()
 			else:
 				bin_min=hN2[seq].GetAxis(H2_DIM['PHI']).FindBin(phi_min+2/2)
 				bin_max=hN2[seq].GetAxis(H2_DIM['PHI']).FindBin(phi_max-2/2)
 				hN2[seq].GetAxis(H2_DIM['PHI']).SetRange(bin_min,bin_max)
 				hTHETA=hN2[seq].Projection(H2_DIM['THETA'])
+				hTHETA.SetName("hTHETA")
 				hTHETA.Write()
 				
-		
-			
-def disp_yield():
+def disp_yields():
         FIN=ROOT.TFile(os.path.join(os.environ['OBSDIR_ELASTIC'],'yield.root'))	
 	OUTDIR=os.path.join(os.environ['OBSDIR_ELASTIC'],'displays')
 	if not os.path.exists(OUTDIR):
@@ -125,57 +100,61 @@ def disp_yield():
 	      #('SF'):ROOT.gROOT.ProcessLine("kRed"),#Using SC instead till Hole Filling 
 	      ('SC'):ROOT.gROOT.ProcessLine("kRed")}
    
-	#! Get all histograms and set up their aesthetics
-	h={}
-        for seq in ['ST','SR','SC','ER','EC']:
-                for var in VARS:
-                        h[seq,var]=FIN.Get("%s/h%s"%(seq,var))
-			h[seq,var].SetLineColor(coll[seq])
-			h[seq,var].SetMarkerColor(coll[seq])
-			h[seq,var].GetXaxis().SetRangeUser(10,50)
-	#! Checkout Simulation
-	c_ST_SR_SC=ROOT.TCanvas()
-	#hSTn=h['ST','THETA'].DrawNormalized("",1000)
-	#hSCn=h['SC','THETA'].DrawNormalized("sames",1000)
-	h['ST','THETA'].Draw()#DrawNormalized("",1000)
-        h['SC','THETA'].Draw("sames")#DrawNormalized("sames",1000)
-        h['SR','THETA'].Draw("sames")
-	#! Set the minimum and maximum of y coordinate of histograms
-	#maxl=[hSTn.GetMaximum(),hSCn.GetMaximum()]
-	#maxl=[h['ST','THETA'].GetMaximum(),h['SC','THETA'].GetMaximum(),h['SR','THETA'].GetMaximum()]
-	#maximum=max(maxl)
-	#for htmp in [hSTn,hSCn]:
-	#for htmp in [h['ST','THETA'],h['SC','THETA'],h['SR','THETA']]:
-		#htmp.SetMinimum(0.)
-		#htmp.SetMaximum(maximum+10)
-	c_ST_SR_SC.SaveAs("%s/c_ST_SR_SC.png"%OUTDIR)	
+	#! Get all hTHETA in PHI_PROJ_BINS and set up their aesthetics
+	for binnum in PHI_PROJ_BINS:
+		hTHETA={}
+		for seq in ['ST','SR','SC','ER','EC']:	
+                        print "binnum=",binnum
+                        hTHETA[seq]=FIN.Get("%s/phibin%d/hTHETA"%(seq,binnum))
+			hTHETA[seq].SetLineColor(coll[seq])
+			hTHETA[seq].SetMarkerColor(coll[seq])
+			hTHETA[seq].GetXaxis().SetRangeUser(10,50)
+		outdir=os.path.join(OUTDIR,"phibin%d"%binnum)
+        	if not os.path.exists(outdir):
+                	os.makedirs(outdir)
+		#! Checkout Simulation
+		c_ST_SR_SC=ROOT.TCanvas()
+		#hSTn=h['ST','THETA'].DrawNormalized("",1000)
+		#hSCn=h['SC','THETA'].DrawNormalized("sames",1000)
+		hTHETA['ST'].Draw()#DrawNormalized("",1000)
+        	hTHETA['SC'].Draw("sames")#DrawNormalized("sames",1000)
+        	hTHETA['SR'].Draw("sames")
+		#! Set the minimum and maximum of y coordinate of histograms
+		#maxl=[hSTn.GetMaximum(),hSCn.GetMaximum()]
+		#maxl=[h['ST','THETA'].GetMaximum(),h['SC','THETA'].GetMaximum(),h['SR','THETA'].GetMaximum()]
+		#maximum=max(maxl)
+		#for htmp in [hSTn,hSCn]:
+		#for htmp in [h['ST','THETA'],h['SC','THETA'],h['SR','THETA']]:
+			#htmp.SetMinimum(0.)
+			#htmp.SetMaximum(maximum+10)
+		c_ST_SR_SC.SaveAs("%s/c_ST_SR_SC.png"%outdir)	
 
-	#! Checkout that the distributions for ER and SR match
-	c_ER_SR=ROOT.TCanvas()
-        hERn=h['ER','THETA'].DrawNormalized("",1000)
-        hSRn=h['SR','THETA'].DrawNormalized("sames",1000)
-	#print hSRn.GetMaximum()
-        #! Set the minimum and maximum of y coordinate of histograms
-        maxl=[hERn.GetMaximum(),hSRn.GetMaximum()]
-        maximum=max(maxl)
-	print maxl,maximum
-        for htmp in [hERn,hSRn]:
-                htmp.SetMinimum(0.)
-                htmp.SetMaximum(maximum+10)
-        c_ER_SR.SaveAs("%s/c_ER_SR.png"%OUTDIR)  
+		#! Checkout that the distributions for ER and SR match
+		c_ER_SR=ROOT.TCanvas()
+        	hERn=hTHETA['ER'].DrawNormalized("",1000)
+        	hSRn=hTHETA['SR'].DrawNormalized("sames",1000)
+		#print hSRn.GetMaximum()
+        	#! Set the minimum and maximum of y coordinate of histograms
+        	maxl=[hERn.GetMaximum(),hSRn.GetMaximum()]
+        	maximum=max(maxl)
+		print maxl,maximum
+        	for htmp in [hERn,hSRn]:
+                	htmp.SetMinimum(0.)
+                	htmp.SetMaximum(maximum+10)
+        	c_ER_SR.SaveAs("%s/c_ER_SR.png"%outdir)  
 
-	#! Checkout that the distributions for EC and SC match
-        c_EC_SC=ROOT.TCanvas()
-        hECn=h['EC','THETA'].DrawNormalized("",1000)
-        hSCn=h['SC','THETA'].DrawNormalized("sames",1000)
-        #print hSCn.GetMaximum()
-        #! Set the minimum and maximum of y coordinate of histograms
-        maxl=[hECn.GetMaximum(),hSCn.GetMaximum()]
-        maximum=max(maxl)
-        print maxl,maximum
-        for htmp in [hECn,hSCn]:
-                htmp.SetMinimum(0.)
-                htmp.SetMaximum(maximum+10)
-        c_EC_SC.SaveAs("%s/c_EC_SC.png"%OUTDIR)
+		#! Checkout that the distributions for EC and SC match
+        	c_EC_SC=ROOT.TCanvas()
+        	hECn=hTHETA['EC'].DrawNormalized("",1000)
+        	hSCn=hTHETA['SC'].DrawNormalized("sames",1000)
+        	#print hSCn.GetMaximum()
+        	#! Set the minimum and maximum of y coordinate of histograms
+        	maxl=[hECn.GetMaximum(),hSCn.GetMaximum()]
+        	maximum=max(maxl)
+        	print maxl,maximum
+        	for htmp in [hECn,hSCn]:
+                	htmp.SetMinimum(0.)
+                	htmp.SetMaximum(maximum+10)
+        	c_EC_SC.SaveAs("%s/c_EC_SC.png"%outdir)
 
 	
