f8f7e863a01b23856ef68d344c96f5d2345df612
Arjun <arjuntvd@gmail.com>
2015-03-19 11:31:29 -0400

    updates

--- sub_studies/study_det_ineff/plot_theta_V_p.C
+++ sub_studies/study_det_ineff/plot_theta_V_p.C
@@ -1,4 +1,40 @@
-void plot_theta_V_p(int top=1,bool exp=kTRUE){
+/*#include <TCut.h>
+#include <TH2F.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <TSystem.h>
+#include <TStyle.h>
+#include <TTree.h>
+#include <TF1.h>
+#include <TString.h>
+
+#include <iostream>
+using namespace std;*/
+
+TString prtcl[]={"e","p","pip","pim"};
+TF1* cut_lw[4][6];
+TF1* cut_hg[4][6];
+void setup_cuts(){
+  for (int i=0;i<4;i++){
+    for (int isctr=0;isctr<6;isctr++){
+      TString name=TString::Format("%s_%d",prtcl[i].Data(),isctr+1).Data();
+      if (prtcl[i]=="e"){
+        if (isctr+1==1||isctr+1==2||isctr+1==5||isctr+1==6){//! no cuts
+          cut_lw[i][isctr]=new TF1(TString::Format("%s_lw",name.Data()),"0", 0,5);
+          cut_hg[i][isctr]=new TF1(TString::Format("%s_hg",name.Data()),"60",0,5);
+        }else if (isctr+1==3){
+          cut_lw[i][isctr]=new TF1(TString::Format("%s_lw",name.Data()),"30-1*x",2.0,3.5);
+          cut_hg[i][isctr]=new TF1(TString::Format("%s_hg",name.Data()),"32-1*x",2.0,3.5);        
+        }else if (isctr+1==4){
+          cut_lw[i][isctr]=new TF1(TString::Format("%s_lw",name.Data()),"21.0-1*x",2.0,4.5);
+          cut_hg[i][isctr]=new TF1(TString::Format("%s_hg",name.Data()),"24.5-1*x",2.0,4.5);
+        }
+      }
+    }
+  }
+}
+//TString prtcl[]={"e","p","pip","pim"};
+void plot_theta_V_p(int top=1,bool exp=kTRUE,bool draw_cut=kFALSE){
   cout<<"top="<<top<<endl;
   TFile* fin;
   TFile* fout;
@@ -11,12 +47,15 @@ void plot_theta_V_p(int top=1,bool exp=kTRUE){
     datadir=gSystem->ExpandPathName("$D2PIDIR_SIM");
   }
   fin=new TFile(TString::Format("%s/mon_det_ineff/d2piRmon.root",datadir.Data()));
-  TTree* t=fin->Get("d2piR/tR");
+  TTree* t=(TTree*)fin->Get("d2piR/tR");
   //cout<<"Tree name:"<<t->GetTitle()<<endl;
   fout=new TFile(TString::Format("%s/mon_det_ineff/thetaVp_top%d.root",datadir.Data(),top),"RECREATE");
 
-  gStyle->SetOptStat(0);
-  TString prtcl[]={"e","p","pip","pim"};
+  if (draw_cut){
+    setup_cuts();
+  }
+
+  //TString prtcl[]={"e","p","pip","pim"};
   TCanvas *c[4];
   TCanvas *clog[4];
   for (int i=0;i<4;i++){
@@ -34,6 +73,7 @@ void plot_theta_V_p(int top=1,bool exp=kTRUE){
   Int_t p_min[]={1,0,0,0};
   Int_t p_max[]={5,4,3,3};
 
+  gStyle->SetOptStat(0);
   TCut cut_top(TString::Format("top==%d",top));
   for (int i=0;i<4;i++){
     for (int isctr=0;isctr<6;isctr++){
@@ -48,12 +88,24 @@ void plot_theta_V_p(int top=1,bool exp=kTRUE){
                                    p_min[i],p_max[i],theta_min[i],theta_max[i]);*/
       cout <<"cmd="<<cmd.Data()<<endl;
       t->Draw(cmd,cut_sctr&&cut_top,"colz");
+      if (draw_cut){
+        if (prtcl[i]=="e"){
+          cut_lw[i][isctr]->Draw("same");
+          cut_hg[i][isctr]->Draw("same");
+        }
+      }
       TH2F* h=(TH2F*)gDirectory->Get(hname);
       cout<<"h = "<<h->GetName()<<endl;      
       //! Draw histogram in log scale
-      TPad* pad=clog[i]->cd(isctr+1);
+      TPad* pad=(TPad*)clog[i]->cd(isctr+1);
       pad->SetLogz(); 
       h->Draw("colz");
+      if (draw_cut){
+        if (prtcl[i]=="e"){
+          cut_lw[i][isctr]->Draw("same");
+          cut_hg[i][isctr]->Draw("same");
+        }
+      }
       //! Write hist to file
       fout->Write(h->GetName(),TObject::kWriteDelete);
     }
