1c99e193041a5723dca73e590ae315871c794112
Arjun <arjuntvd@gmail.com>
2015-03-26 13:36:36 -0400

    updates

--- proc_delast.h
+++ proc_delast.h
@@ -324,7 +324,7 @@ bool ProcDelast::passEvent(bool ismc/*=kFALSE*/){
 			ret=kTRUE;
 		}
 	}else{
-		if (de->MMep<0.1 && de->W<1.1){
+		if (de->MMep<0.1 && de->W<1.0){//! tried W values:1.1
 			ret=kTRUE;
 		}
 	}
