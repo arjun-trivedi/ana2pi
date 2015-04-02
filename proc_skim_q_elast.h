e78d45ae33ffa39b9deaf4881c98a79e6bac5400
Arjun <arjuntvd@gmail.com>
2015-04-02 09:29:26 -0400

    updates

--- proc_skim_q_elast.h
+++ proc_skim_q_elast.h
@@ -12,9 +12,7 @@ using namespace ParticleConstants;
 [03-08-15]
 + Pass Events only if:
 	1. The 1st particle is an Electron, AND
-	2. The total number of Reconstructed tracks is < 4, AND
-	3. The charges of the Reconstructed tracks are identified as:
-		+ 1POS+0NEG+0NEUTRAL
+	2. At least 1POS particle is identified
 *******************************************************/
 
 
@@ -28,10 +26,8 @@ public:
 	void handle();
 	
 protected:
-	/*static const Int_t NUM_EVTCUTS=4;
-	enum { EVT_NULL, EVT, EVT_1ST_E, EVT_LT_EQ_4, EVT_1POS_EX};*/
-        static const Int_t NUM_EVTCUTS=3;
-        enum { EVT_NULL, EVT, EVT_1ST_E, EVT_1POS};
+	static const Int_t NUM_EVTCUTS=3;
+    enum { EVT_NULL, EVT, EVT_1ST_E, EVT_EQGT_1POS};
 };
 
 ProcSkimQElast::ProcSkimQElast(TDirectory *td,DataH10* dataH10,DataAna* dataAna) 
@@ -41,9 +37,7 @@ ProcSkimQElast::ProcSkimQElast(TDirectory *td,DataH10* dataH10,DataAna* dataAna)
 	hevtsum = new TH1D("hevtsum","Event Statistics",NUM_EVTCUTS,0.5,NUM_EVTCUTS+0.5);
 	hevtsum->GetXaxis()->SetBinLabel(EVT,"Total");
 	hevtsum->GetXaxis()->SetBinLabel(EVT_1ST_E,"e^{-} 1^{st}");
-	//hevtsum->GetXaxis()->SetBinLabel(EVT_LT_EQ_4,"gpart <= 4");
-	//hevtsum->GetXaxis()->SetBinLabel(EVT_1POS_EX,"X^{+}");
-	hevtsum->GetXaxis()->SetBinLabel(EVT_1POS,"X^{+} >= 1");
+	hevtsum->GetXaxis()->SetBinLabel(EVT_EQGT_1POS,"X^{+} >= 1");
 }
 
 ProcSkimQElast::~ProcSkimQElast()
@@ -60,30 +54,23 @@ void ProcSkimQElast::handle()
 	hevtsum->Fill(EVT);
 	if ( dH10->id[0]==ELECTRON ) {
 		hevtsum->Fill(EVT_1ST_E);
-		//if ( dH10->gpart<=4) { 
-		    //hevtsum->Fill(EVT_LT_EQ_4);
-			Int_t numPos=0;
-			Int_t numNeg=0;
-			Int_t num0=0;
-			for (Int_t i=1;i<dH10->gpart;i++) {
-			    if      (dH10->q[i]==1)  numPos++;
-			    else if (dH10->q[i]==-1) numNeg++;
-			    else if (dH10->q[i]==0)  num0++;
-			}
-			Int_t binNum = 0;
-			/*if (numPos==1 && numNeg==0 && num0==0) {	
-				binNum=hevtsum->Fill(EVT_1POS_EX);
-				dAna->skimq_elast.isEVT_1POS_EX=kTRUE;
-			}*/		
-			if (numPos>=1) {
-                                binNum=hevtsum->Fill(EVT_1POS);
-                                dAna->skimq_elast.isEVT_1POS_EX=kTRUE;
-                        }	
-			if (binNum>0) {
-				pass=kTRUE;
-				EpProcessor::handle();
-			}
-		//} 
+		Int_t numPos=0;
+		Int_t numNeg=0;
+		Int_t num0=0;
+		for (Int_t i=1;i<dH10->gpart;i++) {
+		    if      (dH10->q[i]==1)  numPos++;
+		    else if (dH10->q[i]==-1) numNeg++;
+		    else if (dH10->q[i]==0)  num0++;
+		}
+		Int_t binNum = 0;
+		if (numPos>=1) {
+			binNum=hevtsum->Fill(EVT_EQGT_1POS);
+			dAna->skimq_elast.isEVT_EQGT_1POS=kTRUE;
+		}	
+		if (binNum>0) {
+			pass=kTRUE;
+			EpProcessor::handle();
+		}
 	}
 }
 
