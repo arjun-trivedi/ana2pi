9b9b5fe129ca11ff12f19d52c009986e4d6b7738
Arjun <arjuntvd@gmail.com>
2015-07-13 19:07:18 -0400

    Work in progress: Re-visit EID cuts

--- sub_studies/study_eid/plot_eid.C
+++ sub_studies/study_eid/plot_eid.C
@@ -17,7 +17,7 @@ TH2F* norm(TH2F* h2){
   return h2c;
 }
 
-void plot_eid(TString slctn,TString seq,int nentries=1000000){
+void plot_eid(TString slctn,TString seq,int nentries=1000000000){
   const int NDTYP=2;
   enum {EXP,SIM};
   TFile* f[NDTYP];// 0=Exp,1=Sim
@@ -66,18 +66,18 @@ void plot_eid(TString slctn,TString seq,int nentries=1000000){
   eid_tool[SIM]=new Eid(TString::Format("%s/ana2pi/eid/eid.mc.out",wspace.Data()).Data());
 
   const int NHST=4;//removed "hsfoutVsfin","hetotVp","ecU","ecV","ecW"
-  TString hst_name[]={"heoutVein","hsftotVp",             "hnphe","hCCthetaVCCseg"};
-  TString hst_title[]={"#E_{out} vs. #E_{in}","hsftotVp", "hnphe","hCCthetaVCCseg"};
+  TString hst_name[]={"heoutVein","hsfoutVsfin","hsftotVp",             "hnphe","hCCthetaVCCseg"};
+  TString hst_title[]={"#E_{out} vs. #E_{in}","hsfoutVsfin","hsftotVp", "hnphe","hCCthetaVCCseg"};
   TString hst_drw_cmd[]={
                          TString::Format("ec_eo:ec_ei>>%s(150,0,1,150,0,1)",        hst_name[0].Data()),
-                         //TString::Format("ec_eo/p:ec_ei/p>>%s(150,0,0.5,150,0,0.5)",hst_name[1].Data()),
-                         TString::Format("etot/p:p>>%s(160,0,5,150,0,0.5)",         hst_name[1].Data()),
+                         TString::Format("ec_eo/p:ec_ei/p>>%s(150,0,0.5,150,0,0.5)",hst_name[1].Data()),
+                         TString::Format("etot/p:p>>%s(160,0,5,150,0,0.5)",         hst_name[2].Data()),
                          //TString::Format("etot:p>>%s(160,0,5,150,0,2)",             hst_name[3].Data()),
                          //TString::Format("ecU>>%s(100,0,450)",                        hst_name[2].Data()), 
                          //TString::Format("ecV>>%s(100,0,450)",                        hst_name[3].Data()),
                          //TString::Format("ecW>>%s(100,0,450)",                        hst_name[4].Data()),
-                         TString::Format("nphe>>%s(100,0,300)",                     hst_name[2].Data()),
-                         TString::Format("cc_theta:cc_segm>>%s(20,0,20,100,0,50)",   hst_name[3].Data())
+                         TString::Format("nphe>>%s(100,0,300)",                     hst_name[3].Data()),
+                         TString::Format("cc_theta:cc_segm>>%s(20,0,20,100,0,50)",   hst_name[4].Data())
                          };
   
   const int NSCTR=6;
@@ -94,6 +94,8 @@ void plot_eid(TString slctn,TString seq,int nentries=1000000){
     for (int ihst=0;ihst<NHST;ihst++){// begin nhst loop
       //if (ihst>1)continue;
       for (int isctr=0;isctr<NSCTR;isctr++){ //begin sctr loop
+        TString outdir=TString::Format("%s/ana2pi/sub_studies/study_eid/hists/%s/%s/%s/%s",wspace.Data(),slctn.Data(),seq.Data(),dtyp_name[idtyp].Data(),hst_name[ihst].Data());
+        gSystem->mkdir(outdir,1);
         c1->cd();
         TCut sctr_cut=TString::Format("sector==%d",isctr+1);
         TCut cut=pre_cut&&sctr_cut&&slctn_cut;
@@ -101,7 +103,7 @@ void plot_eid(TString slctn,TString seq,int nentries=1000000){
         t[idtyp]->Draw(hst_drw_cmd[ihst],cut,"colz",nentries);
    
         gStyle->SetOptStat("ne");//mri"); 
-        if (ihst<=1||ihst==3){//! i.e. 2D hists
+        if (ihst<=2||ihst==4){//! i.e. 2D hists
           TH2F* h2tmp=(TH2F*)gDirectory->Get(hst_name[ihst]);
           h[idtyp][ihst][isctr]=(TH2F*)h2tmp->Clone();
         }else{//! i.e. 1D hists
@@ -110,16 +112,23 @@ void plot_eid(TString slctn,TString seq,int nentries=1000000){
         }
         h[idtyp][ihst][isctr]->SetName(TString::Format("%s_%s_s%d",       hst_name[ihst].Data(),dtyp_name[idtyp].Data(),isctr+1));
         h[idtyp][ihst][isctr]->SetTitle(TString::Format("%s_%s", pre_cut.GetTitle(),slctn.Data()));
+        if (ihst<=2||ihst==4){//! i.e. 2D hists
+          h[idtyp][ihst][isctr]->Draw("colz");
+        }else{//! i.e. 1D hists
+          h[idtyp][ihst][isctr]->Draw();
+        }
+        if (ihst==2)eid_tool[idtyp]->DrawSFcuts(isctr+1);
+        c1->SaveAs(TString::Format("%s/%s.jpg",outdir.Data(),h[idtyp][ihst][isctr]->GetName()));
       }//end sctr loop
     }// end nhst loop
   }//end dtyp loop
 
-  //! Now plot hists
+  //! Now plot hists for intereactive viewing 
   //! Seperate canvases for each dtyp for 2D
   for (int idtyp=0;idtyp<NDTYP;idtyp++){
     for (int ihst=0;ihst<NHST;ihst++){
       //if (ihst>1) continue;
-      if (ihst>1&&ihst!=3) continue;
+      if (ihst>2&&ihst!=4) continue;
       cout<<hst_name[ihst]<<endl;
       TString cname=TString::Format("c_%s_%s",dtyp_name[idtyp].Data(),hst_name[ihst].Data());
       c[idtyp][ihst]=new TCanvas(cname,cname);
@@ -128,23 +137,24 @@ void plot_eid(TString slctn,TString seq,int nentries=1000000){
         c[idtyp][ihst]->cd(isctr+1);
         //h[idtyp][ihst][isctr]=norm(h[idtyp][ihst][isctr]);
         h[idtyp][ihst][isctr]->Draw("colz");
-        if (ihst==1)eid_tool[idtyp]->DrawSFcuts(isctr+1);
+        if (ihst==2)eid_tool[idtyp]->DrawSFcuts(isctr+1);
       }
     }
   }
 
   //! Same canvas for each for 1D
   for (int ihst=0;ihst<NHST;ihst++){
-    if (ihst<=1||ihst==3) continue;
+    if (ihst<=2||ihst==4) continue;
     TString cname=TString::Format("c_%s",hst_name[ihst].Data());
     c[EXP][ihst]=new TCanvas(cname,cname);
     c[EXP][ihst]->Divide(3,2);
     for (int isctr=0;isctr<NSCTR;isctr++){
       c[EXP][ihst]->cd(isctr+1);
-      h[EXP][ihst][isctr]->SetLineColor(kBlue);
+      h[EXP][ihst][isctr]->Draw();
+      /*h[EXP][ihst][isctr]->SetLineColor(kBlue);
       h[SIM][ihst][isctr]->SetLineColor(kRed);
       h[EXP][ihst][isctr]->DrawNormalized("",1000);
-      h[SIM][ihst][isctr]->DrawNormalized("sames",1000);
+      h[SIM][ihst][isctr]->DrawNormalized("sames",1000);*/
     }
   }
 }
