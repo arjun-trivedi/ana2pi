3b48353dcacbd90891f30e3e23648e53c885ed8d
Arjun <arjuntvd@gmail.com>
2015-07-17 12:01:55 -0400

    Work in progress: Revisit electron selection: Organizational changes:Added study_eid(efid)/setup.tcshrc and removed epconfig_cpp.d, cuts_h.d

--- epconfig_cpp.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-/home/trivedia/CLAS/workspace/ana2pi/epconfig_cpp.so: /home/trivedia/CLAS/workspace/ana2pi/epconfig.h
-/home/trivedia/CLAS/workspace/ana2pi/epconfig_cpp.so: /opt/root/include/cintdictversion.h /opt/root/include/RVersion.h
-epconfig_cpp__ROOTBUILDVERSION= 5.33/03
