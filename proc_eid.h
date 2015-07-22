8ec4bf3c008ce9fb21bdd6f6b34d100ee523e4e3
Arjun <arjuntvd@gmail.com>
2015-07-22 07:35:13 -0400

    WIP: Re-visit electron selection: study_efid: Update to infrastructure(Part 2)

--- proc_eid.h
+++ proc_eid.h
@@ -212,7 +212,7 @@ Bool_t ProcEid::goodE(){
 										if (_eidTool->PassThreshold(eid->p)) {
 											hevtsum->Fill(EVT_ECLOW1);
 											float uvw[3]={eid->ecU,eid->ecV,eid->ecW};
-											if (_eidTool->PassECFid(uvw)){
+											//if (_eidTool->PassECFid(uvw)){
 												hevtsum->Fill(EVT_ECFID);
 												Int_t sector = eid->sector;
 												Float_t p = eid->p;
@@ -224,7 +224,7 @@ Bool_t ProcEid::goodE(){
 													}
 													retval = kTRUE;
 												}
-											}
+											//}
 										}
 									}
 								//}
