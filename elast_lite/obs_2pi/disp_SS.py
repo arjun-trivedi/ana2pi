a5f027f6f8e5014c3d9e9cfd1e998c3b3d6692e0
Arjun <arjuntvd@gmail.com>
2016-04-19 12:49:13 -0400

    On branch test_2pi_xsec_lite_101115 Changes to be committed: 1. (Minor) Update related to previos commit where in the process of h10_2_Obs, in the SS part, I forgot to update the script to do SS for Obs_R2. 	modified:   elast_lite/obs_2pi/disp_SS.py

--- elast_lite/obs_2pi/disp_SS.py
+++ elast_lite/obs_2pi/disp_SS.py
@@ -11,5 +11,11 @@ def plot_SS(q2min,q2max,wmin,wmax,expt,dbg,obsd,SS_tag):
 		print obsnum,obsdir,obstag
 	print "SS_tag=",SS_tag
 
-	do=DispObs('NA','NA','NA',q2min,q2max,wmin,wmax,expt,dbg,obsd=obsd,SS_tag=SS_tag)
-	do.disp_1D_SS()
+	#! Just to be safe, in the sense of resources (mainly memory), use a new instance of
+	#! of DispObs for disp_1D_SS() and disp_R2_SS()
+	#! 1. First do disp_1D_SS()
+	do1=DispObs('NA','NA','NA',q2min,q2max,wmin,wmax,expt,dbg,obsd=obsd,SS_tag=SS_tag)
+	do1.disp_1D_SS()
+	#! 2. Now do disp_R2_SS()
+	do2=DispObs('NA','NA','NA',q2min,q2max,wmin,wmax,expt,dbg,obsd=obsd,SS_tag=SS_tag)
+        do2.disp_R2_SS()
