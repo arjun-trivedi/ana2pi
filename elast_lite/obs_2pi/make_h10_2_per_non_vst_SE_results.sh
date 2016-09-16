e572a162524c1bc534f37ce1df10b8c54f020083
Arjun Trivedi <arjuntvd@gmail.com>
2016-09-16 13:45:30 -0400

    On branch test_2pi_xsec_lite_101115 Changes to be committed: 1. Main update: SE-ana-9: 	+ Upgrade of h10->Observables: 		+ Implemented code to make h10->per_non_vst_SE_results 		+ To do: per_non_vst_SE_results->cmb_vst_SE_results (cmb_vst_SE_results=final results)
    	deleted:    obs_2pi/disp_per_non_vst_SE_result.py
    	new file:   obs_2pi/h10_2_per_non_vst_SE_results.py
    	new file:   obs_2pi/make_h10_2_per_non_vst_SE_results.sh

---
+++ elast_lite/obs_2pi/make_h10_2_per_non_vst_SE_results.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#! 1. lowQ2
+#! SSBands,MM
+h10_2_per_non_vst_SE_results.py e16 2.00 3.00 lowQ2_SSBands sim4:sim5:sim6:sim7:sim8:sim13 SSBands &
+h10_2_per_non_vst_SE_results.py e16 2.00 3.00 lowQ2_MM      sim4:sim5:sim6:sim7:sim8:sim13 MM &
+
+#! wait for lowQ2 to finish before moving to highQ2
+wait
+
+#! 2. highQ2
+#! SSBands,MM
+h10_2_per_non_vst_SE_results.py e16 3.00 5.00 highQ2_SSBands sim9:sim10:sim11:sim12 SSBands &
+h10_2_per_non_vst_SE_results.py e16 3.00 5.00 highQ2_MM      sim9:sim10:sim11:sim12 MM &
