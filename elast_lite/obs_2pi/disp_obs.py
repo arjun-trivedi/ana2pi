On branch ananote Changes to be committed: 1. update-ananote-simstats-levels-5: Fixed bug where the Hole fraction from h5-SH was being overestimated due to precision of values that should be 0 and therefore not be counted as a Hole(0=ST-SC=>Not Holes), but were actually of the order of 1*E-7. This non-0 value was leading to an overestimated of the Hole fraction. For details see DispObs::get_sim_stats() 	modified:   elast_lite/obs_2pi/disp_obs.py 	modified:   presentation_plots/make_simtats_presentation.py 	modified:   sub_studies/study_compare_AT_EI_results/study_yields_acceptance.py 	new file:   sub_studies/study_tchncl_dtls/THnSparse_Truth_Table_Filled-Empty-Cells_0-non-0_Cells.txt

elast_lite/obs_2pi/disp_obs.py
@@ -3749,7 +3749,12 @@ class DispObs:
 				#print q2,w
 				#! Determine nbins,N,mu,sg for this q2,w
 				h5=self.FIN.Get("%s/%s/VST1/h5"%(q2w,seq))
-				nbins=thntool.GetNbinsNotEq0(h5)
+				#! [03-12-18 ] Note use of special_case for nbins which is required for SH
+				#!             because for cases when ST-SH should be 0, is not exactly 0 due to
+				#!             precision issues (see documentation in THnTool::GetBinStats())
+				special_case=False
+				if seq=='SH':special_case=True
+				nbins=thntool.GetNbinsNotEq0(h5,special_case)
 				N=thntool.GetIntegral(h5)
 				binc_stats=np.zeros(2,'f')
 				thntool.GetBinContentDistStats(h5,binc_stats)